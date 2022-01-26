f2c77cda9ebc43f37b7550f498d12f7c69fe304f
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-01-25 21:28:34 -0500

    changed pulse envelopes

--- silospin/quantum_compiler/quantumgates.py
+++ silospin/quantum_compiler/quantumgates.py
@@ -1,13 +1,20 @@
 import numpy as np
 from silospin.drivers import zi_mfli_v1
-from silospin.drivers.math import math_helpers
+from silospin.drivers.math.math_helpers import gauss, rectangular
 
 class SingleQubitGate:
-
-    def __init__(self, gate_type, pulse_type="rectangular", hdawg=None,t_p=0):
+    ##hdawg = HdawgDriver(dev_id = "1234")
+    ## X_gate = SingleQubitGate(gate_type="x", pulse_type="rectangular", awg=hdawg)
+    ## X_gate.
+    ##
+    ##
+    ##
+    def __init__(self, gate_type, pulse_type, awg=None, I_channel=1, I_osc=1, I_mod_channel="sin11", mod_freq=10e6, Q_channel=2, Q_osc=2, Q_mod_channel="sin22", IQ_offset=0):
         ##set default values for all input parameters...
         gates = {"x", "y", "wait"}
         pulses = {"rectangular", "gaussian", "chirped", "adiabatic"}
+        ## ensure connection with AWG
+
         try:
             if type(gate_type) is not str:
                 raise TypeError("'gate_type' should be a string.")
@@ -24,13 +31,20 @@ class SingleQubitGate:
         except TypeError:
             raise
 
+        if pulse_type == "rectangular":
+            envelope = rectangular(npoints, amp)
+        elif pulse_type == "gaussian":
+            envelope = gauss(x, amp=amplitude, mu=t_p/2, sig=sigma)
+        else:
+            pass
+
         self._gate_type = gate_type
         self._pulse_type = pulse_type
         self._hdawg = hdawg
-        self._pulse_type = pulse_type
-        self._pulse_envelope = None
-        self._IQ_settings = {"I": {"channel": 1, "osc": 1, "freq": 10e6, "phase_shift": 0, "modulation_channels": "sin11", "wave_out": True}, "Q": {"channel": 2, "osc": 2, "freq": 10e6, "phase_shift": np.pi/2,  "modulation_channels": "sin22", "wave_out": True}}
+        self._pulse_envelope = envelope
+        self._IQ_settings = {"I": {"channel": I_channel, "osc": I_osc, "freq": mod_freq, "phase_shift": 0, "modulation_channels": I_mod_channel, "wave_out": True}, "Q": {"channel": Q_channel, "osc": Q_osc, "freq": mod_freq, "phase_shift": np.pi/2+IQ_offset, "modulation_channels": Q_mod_channel, "wave_out": True}}
 
+    #def set_awg()
 
     def make_pulse_envelope(self, pulse_type, npoints, t_start, t_end, amplitude, t_p=None, mu=None, sig=None):
         pulses  = {"rectangular", "gaussian", "adiabatic", "chirped"}
@@ -42,11 +56,11 @@ class SingleQubitGate:
             pass
         else:
             pass
-        self._pulse_envelope = wave 
-
+        self._pulse_envelope = wave
 
     #def get_pulse_envelope(self)
-    #def make_pulse(self):
+#    def make_pulse(self, gate_type):
+        ##add assertiion that pulse envelope exists
     #def get_pulse(self):
     #def get_IQ_settings(self):
     #def set_IQ_settings(self):
