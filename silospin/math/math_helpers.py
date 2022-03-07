ec35c44ab4d89b1157af33c2598aed56ac7317ec
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-03-07 09:36:08 -0500

    changed math helpers

---
+++ silospin/math/math_helpers.py
@@ -0,0 +1,9 @@
+import numpy as np
+def gauss(npoints, amp, mu, sig):
+    return amp*np.exp(-(x-mu)**2/(2*sig**2))
+
+def rectangular(npoints, amp):
+    return amp*np.ones(npoints)
+
+##def chirped():
+##def adiabatic():
