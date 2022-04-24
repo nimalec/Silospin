95aa73364e59c36e5e028b29b7893fc648c15fe6
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-04-24 16:15:51 -0700

    awg

--- silospin/quantum_compiler/quantumgates.py
+++ silospin/quantum_compiler/quantumgates.py
@@ -290,24 +290,24 @@ class QubitGatesSet:
          awg_index = 0
          daq.setVector(f"/{dev}/awgs/{awg_index}/commandtable/data", json.dumps(self._command_table))
 
-         waveforms = Waveforms()
-         idx = 0
-         for gt in self._gate_string:
-             if gt in {"x", "y", "xxx", "yyy"}:
-                 #waveforms.assign_waveform(idx, self._tau_pi_2_wave)
-                 wave_raw = zhinst.utils.convert_awg_waveform(self._tau_pi_2_wave)
-                 daq.setVector(f'/{dev}/awgs/{awg_index}/waveform/waves/{idx}', wave_raw)
+         # waveforms = Waveforms()
+         # idx = 0
+         # for gt in self._gate_string:
+         #     if gt in {"x", "y", "xxx", "yyy"}:
+         #         waveforms.assign_waveform(idx, self._tau_pi_2_wave)
+         #         wave_raw = zhinst.utils.convert_awg_waveform(self._tau_pi_2_wave)
+         #         daq.setVector(f'/{dev}/awgs/{awg_index}/waveform/waves/{idx}', wave_raw)
+         #
+         #     elif gt in  {"xx", "yy", "mxxm", "myym"}:
+         #         waveforms.assign_waveform(idx, self._tau_pi_wave)
+         #         wave_raw = zhinst.utils.convert_awg_waveform(self._tau_pi_wave)
+         #         daq.setVector(f'/{dev}/awgs/{awg_index}/waveform/waves/{idx}', wave_raw)
+         #     else:
+         #         pass
+         #     idx += 1
+         #
+         # self._waveforms = waveforms
 
-             elif gt in  {"xx", "yy", "mxxm", "myym"}:
-                 #waveforms.assign_waveform(idx, self._tau_pi_wave)
-                 wave_raw = zhinst.utils.convert_awg_waveform(self._tau_pi_wave)
-                 daq.setVector(f'/{dev}/awgs/{awg_index}/waveform/waves/{idx}', wave_raw)
-             else:
-                 pass
-             idx += 1
+    #def run_program(self):
 
-         self._waveforms = waveforms
-         #self._awg._awgs["awg1"].write_to_waveform_memory(self._waveforms)
-         #wave_raw = zhinst.utils.convert_awg_waveform(wave[0])
-         #daq.setVector(f'/{dev}/awgs/{awg_index}/waveform/waves/{i}', wave_raw)
-         #daq.setVector(f"/{dev}/awgs/{awg_index}/commandtable/data", json.dumps(self._command_table))
+    #def plot_waveforms(self):
