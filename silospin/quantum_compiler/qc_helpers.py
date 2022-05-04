ee95b1246051c774da3b7c6f41061e59c4b41a56
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-05-04 08:56:13 -0400

    awg

--- silospin/quantum_compiler/qc_helpers.py
+++ silospin/quantum_compiler/qc_helpers.py
@@ -38,7 +38,7 @@ def make_command_table(gate_string, iq_settings, sample_rate):
         else:
             waveform = {"index": wave_idx[gt], "awgChannel0": ["sigout0","sigout1"]}
             phase0 = {"value": phases[gt]["phase0"] - phi0_prev, "increment": True}
-            phase1 = {"value": phases[gt]["phase1"] - phi1_prev + iq_settings["iq_offset"], "increment": True}
+            phase1 = {"value": phases[gt]["phase1"] + 90 - phi1_prev + iq_settings["iq_offset"], "increment": True}
             phi0_prev = phases[gt]["phase0"]
             phi1_prev = phases[gt]["phase1"]
 
