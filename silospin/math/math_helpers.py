d300f1e61630196f111aa4d56bbf57e83bc1ca32
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-06-04 15:54:28 -0400

    set phase

--- silospin/math/math_helpers.py
+++ silospin/math/math_helpers.py
@@ -4,7 +4,7 @@ def gauss(x, amp, mu, sig):
 
 def rectangular(npoints, amp):
     array = amp*np.ones(npoints)
-    zr = np.zeros(32)
+    zr = np.zeros(16)
     return np.concatenate((zr, array,zr), axis=None)
 
 
