7fbfc33f1ca03cc8158c073bd57936e77a0ac95b
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-06-16 14:18:24 -0400

    rectangular

--- silospin/math/math_helpers.py
+++ silospin/math/math_helpers.py
@@ -2,12 +2,24 @@ import numpy as np
 def gauss(x, amp, mu, sig):
     return amp*np.exp(-(x-mu)**2/(2*sig**2))
 
-def rectangular(npoints, amp):
+def rectangular(npoints, amp, min_points = 48):
     array = amp*np.ones(npoints)
-    return array
-    #zr = np.zeros(16)
-    #return np.concatenate((zr, array,zr), axis=None)
+    if npoints < min_points:
+        npoints_pad = min_points - npoints
+        if npoints_pad%2 == 0:
+            zero_pad_l = np.zeros(int(npoints_pad/2))
+            zero_pad_r = np.zeros(int(npoints_pad/2))
+        elif 2*int(npoints_pad/2) + npoints < min_points:
+            zero_pad_l = np.zeros(int(npoints_pad/2))
+            zero_pad_r = np.zeros(int(npoints_pad/2) + 1)
+        else:
+            zero_pad_l = np.zeros(int(npoints_pad/2))
+            zero_pad_r = np.zeros(int(npoints_pad/2)-1)
+        array = np.concatenate((zero_pad_l, array,zero_pad_r), axis=None)
+    else:
+        array = amp*np.ones(npoints)
 
+    return array
 
 ##def chirped():
 ##def adiabatic():
