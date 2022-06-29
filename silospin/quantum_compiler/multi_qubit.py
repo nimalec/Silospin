c9fee6796aa61da8c0177bba691691b1778b2454
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-06-29 16:10:10 -0400

    gst parsing

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 from math import ceil
 import json
 import time
@@ -9,7 +10,8 @@ import zhinst.utils
 from silospin.drivers.zi_hdawg_driver import HdawgDriver
 from silospin.math.math_helpers import gauss, rectangular
 from silospin.quantum_compiler.qc_helpers import *
-from silospin.io.qc_io import read_qubit_paramater_file, write_qubit_parameter_file
+from silospin.io.qc_io import read_qubit_paramater_file, write_qubit_parameter_file, quantum_protocol_parser
+
 
 class MultiQubitGatesSet:
     def __init__(self, gate_strings, awg, qubit_parameters=None, sample_rate=2.4e9, continuous=False, hard_trigger = False, soft_trigger = False, waveforms_preloaded=False, update_qubit_parameters = "0", qubit_parameters_file_path = "qubit_parameters.csv"):
@@ -205,6 +207,127 @@ class MultiQubitGatesSet:
              self._awg._awgs["awg"+str(awg_idx+1)].enable(True)
              daq.setVector(f"/{dev}/awgs/{awg_idx}/commandtable/data", json.dumps(self._command_tables[str(awg_idx)]))
 
+
+    def run_program(self, awg_idxs=None):
+        if awg_idxs:
+            awg_idxs = awg_idxs
+        else:
+            awg_idxs = self._awg_idxs
+        for idx in awg_idxs:
+            self._awg._awgs["awg"+str(idx+1)].single(True)
+            self._awg._awgs["awg"+str(idx+1)].enable(True)
+
+
+class MultiQubitGST:
+    def __init__(self, gst_file_path, awg, qubits = {1,2,3,4}):
+        #specify input parameters
+        self._gst_path = gst_file_path
+        self._awg = awg
+        self._sample_rate = 2.4e9
+        self._awg_cores =  np.array(list(qubits))-1
+        self._qubit_parameters = {
+        "0": {"i_amp_pi": 0.5, "q_amp_pi": 0.5 , "i_amp_pi_2": 0.5, "q_amp_pi_2": 0.5, "tau_pi" : 100e-9,  "tau_pi_2" :  50e-9,  "delta_iq" : 0 , "mod_freq": 60e6},
+        "1": {"i_amp_pi": 0.5, "q_amp_pi": 0.5 , "i_amp_pi_2": 0.5, "q_amp_pi_2": 0.5, "tau_pi" : 100e-9,  "tau_pi_2" :  50e-9,  "delta_iq" : 0 , "mod_freq": 60e6},
+        "2": {"i_amp_pi": 0.5, "q_amp_pi": 0.5 , "i_amp_pi_2": 0.5, "q_amp_pi_2": 0.5, "tau_pi" : 100e-9,  "tau_pi_2" :  50e-9,  "delta_iq" : 0 , "mod_freq": 60e6},
+        "3": {"i_amp_pi": 0.5, "q_amp_pi": 0.5 , "i_amp_pi_2": 0.5, "q_amp_pi_2": 0.5, "tau_pi" : 100e-9,  "tau_pi_2" :  50e-9,  "delta_iq" : 0 , "mod_freq": 60e6}}
+        qubit_lengths = {}
+        for idx in qubits:
+            qubit_lengths[idx]["pi"] = self._qubit_parameters[str(idx-1)]["tau_pi"]
+            qubit_lengths[idx]["pi_2"] = self._qubit_parameters[str(idx-1)]["tau_pi_2"]
+
+        #input gate sequence
+        self._gate_sequences = quantum_protocol_parser(self._gst_path, qubit_lengths = qubit_lengths, qubit_set = qubits)
+
+
+        gts_0 = [] #initial gates
+        n_waits = {0: [], 1: [], 2: [], 3: []} #wait times
+        cts_idxs_all = {} # command table indices, ct_idxs_all[line][awg_idx]
+        command_tables = {} # command tables for each core, command_tables[awg_idx]
+        #n_t_t = 0 #number of play_zero instances accumulated across all lines
+
+        n_t_ts = {0: 0, 1: 0, 2: 0, 3: 0}
+
+        #Loop over number of lines in sequence file
+        for idx in self._gate_sequences:
+            #Define dict for ct_idx of each line, cts_idxs[awg_idx]
+            cts_idxs = {}
+            gate_sequence = self._gate_sequence[idx] #sequence dictionary for a line
+            gts_0.append(gate_sequence[0]) ##append initial gate sequence
+
+            for key in gate_sequence: #loops over qubits
+                gt_seq = gate_strings[key] #gt sequence for qubit (list)
+                if gt_seq:
+                    ct_idxs, n_ts = make_command_table_idxs_v2(len(self._gate_sequences), n_t_ts[key], gt_seq[1:len(gt_seq)], self._sample_rate)
+                    n_waits[key].append(n_ts)
+                    cts_idxs[key] = ct_idxs
+                    n_t_ts[key] = n_t_ts[key] + len(n_ts)
+                else:
+                    pass
+            ct_idxs_all[idx] = cts_idxs
+
+        for key in n_waits:
+            if n_waits[key]:
+                n_waits[key] = list(np.array(n_waits[key]).flatten())
+            else:
+                pass
+
+        command_tables = {}
+        for i in self._awg_cores:
+            command_table = generate_reduced_command_table(gts_0, npoints_wait = n_waits, npoints_plunger = None, delta_iq = self._qubit_parameters[str(i)]["delta_iq"], phi_z = 0)
+            self._command_tables[str(i)] = command_table
+
+        self._command_tables = command_tables
+
+        waveforms_tau_pi = {}
+        waveforms_tau_pi_2 = {}
+        waveforms_qubits = {}
+
+        seq_code = {}
+        command_code = {}
+        for awg_idx in self._awg_idxs:
+            n_array = []
+            waveforms = Waveforms()
+            npoints_tau_pi = ceil(self._sample_rate*self._qubit_parameters[str(awg_idx)]["tau_pi"]/16)*16
+            npoints_tau_pi_2 = ceil(self._sample_rate*self._qubit_parameters[str(awg_idx)]["tau_pi_2"]/16)*16
+            waveforms_tau_pi[str(awg_idx)] = rectangular(npoints_tau_pi, self._qubit_parameters[str(awg_idx)]["i_amp_pi"])
+            waveforms_tau_pi_2[str(awg_idx)] = rectangular(npoints_tau_pi_2, self._qubit_parameters [str(awg_idx)]["i_amp_pi_2"])
+            n_array.append(len(waveforms_tau_pi[str(awg_idx)]))
+            n_array.append(len(waveforms_tau_pi_2[str(awg_idx)]))
+            waveforms.assign_waveform(slot = 0, wave1 = waveforms_tau_pi[str(awg_idx)])
+            waveforms.assign_waveform(slot = 1, wave1 = waveforms_tau_pi_2[str(awg_idx)])
+            waveforms_qubits[str(awg_idx)] = waveforms
+            seq_code[awg_idx] =  make_waveform_placeholders(n_array)
+            command_code[awg_idx] = []
+            for line in cts_idxs_all:
+                n_seq =
+                command_code[awg_idx].append(make_gateset_sequencer_fast_v2(line_idx, line))
+
+
+
+
+
+
+        for awg_idx in self._awg_idxs:
+            self._awg.load_sequence(seq_code, awg_idx=awg_idx)
+            self._awg._awgs["awg"+str(awg_idx+1)].write_to_waveform_memory(waveforms)
+
+        self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
+        self._channel_osc_idxs = {"0": 1, "1": 5, "2": 9, "3": 13}
+
+        daq = self._awg._daq
+        dev = self._awg._connection_settings["hdawg_id"]
+        for awg_idx in self._awg_idxs:
+             i_idx = self._channel_idxs[str(awg_idx)][0]
+             q_idx = self._channel_idxs[str(awg_idx)][1]
+             osc_idx = self._channel_osc_idxs[str(awg_idx)]
+             self._awg._hdawg.sigouts[i_idx].on(1)
+             self._awg._hdawg.sigouts[q_idx].on(1)
+             self._awg.set_osc_freq(osc_idx, self._qubit_parameters[str(awg_idx)]["mod_freq"])
+             self._awg.set_sine(i_idx+1, osc_idx)
+             self._awg.set_sine(q_idx+1, osc_idx)
+             self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[str(awg_idx)]["i_amp_pi"])
+             self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[str(awg_idx)]["q_amp_pi"])
+             daq.setVector(f"/{dev}/awgs/{awg_idx}/commandtable/data", json.dumps(self._command_tables[str(awg_idx)]))
 
     def run_program(self, awg_idxs=None):
         if awg_idxs:
--- silospin/quantum_compiler/qc_helpers.py
+++ silospin/quantum_compiler/qc_helpers.py
@@ -118,18 +118,18 @@ def make_gateset_sequencer_fast(n_array, n_seq, continuous=False, soft_trigger=F
         if hard_trigger is True:
             program = sequence_code + "waitDigTrigger(1);\n setDIO(1);\n wait(2);\nsetDIO(0);\nwaitDIOTrigger();\n" + command_code
         elif soft_trigger is True:
-            #program = sequence_code + "waitDigTrigger(1);\n setDIO(1);\n wait(2);\nsetDIO(0);\nwaitDIOTrigger();\n" + command_code
             program = sequence_code + "setTrigger(1);\nsetTrigger(0);\n" + command_code
-
-        #if soft_trigger is False or hard_trigger is False:
-    #        program = sequence_code + command_code
-#        elif soft_trigger is True:
-#            program = sequence_code + "setTrigger(1);\nsetTrigger(0);\n" + command_code
-            #program = sequence_code + "waitTrigger(0, 0.2);\n" + command_code
-#        elif hard_trigger is True:
-#            program = sequence_code + "waitTrigger(1, 0.2);\n" + command_code
-
     return program
+
+def make_gateset_sequencer_fast_v2(line_idx, n_seq):
+    command_code = "executeTableEntry("+str(line_idx)+");\n"
+    for n in n_seq:
+        idx_str = str(n)
+        line = "executeTableEntry("+idx_str+");\n"
+        command_code = command_code + line
+    program = "setTrigger(1);\nsetTrigger(0);\n" + command_code
+    return program
+
 
 def generate_reduced_command_table(gt_0, npoints_wait = [], npoints_plunger = None, delta_iq = 0, phi_z = 0):
     ## also add I/Q correction here
@@ -181,6 +181,60 @@ def generate_reduced_command_table(gt_0, npoints_wait = [], npoints_plunger = No
     command_table  = {'$schema': 'https://json-schema.org/draft-04/schema#', 'header': {'version': '0.2'}, 'table': ct}
     return command_table
 
+def generate_reduced_command_table_v2(gts_0, npoints_wait = [], npoints_plunger = None, delta_iq = 0, phi_z = 0):
+    ## also add I/Q correction here
+
+    ## npoints_wait: list of number of points for various wait durations
+    ## phi_z: phase correction
+    ## Note: assumes wv_idx = 0 corresponds to tau_pi_2, wv_idx_2 correspodns to pi
+    ## ct_idx = 0-N ==> number of
+    ## ct_idx = 1-7 ==> pi_2 pulses (0,90, 180, 270, -90, -180, -270)
+    ## ct_idx = 8-14 ==> pi pulses
+    ## ct_idx = 15-21 ==> z gates
+    ## ct_idx = 22 - N ==> waits
+    ## ct_idx = N+1 - M ==> plunger gates
+
+    ##Add plunger, wait to initial gates
+    initial_gates = {"x": {"phi": 0, "wave_idx": 0}, "y": {"phi": 90, "wave_idx": 0}, "xxx": {"phi": 180, "wave_idx": 0}, "yyy": {"phi": -90, "wave_idx": 0}, "xx": {"phi": 0, "wave_idx": 1}, "yy": {"phi": 90, "wave_idx": 1}, "mxxm": {"phi": 180, "wave_idx": 1}, "myym": {"phi": -90, "wave_idx": 1}}
+
+    ct = []
+    n_phases = 7
+
+    idx = 0
+    for gt in gts_0:
+        waveform_0 = {"index": initial_gates[gt]["wave_idx"], "awgChannel0": ["sigout0","sigout1"]}
+        phase_0_0 = {"value": initial_gates[gt]["phi"], "increment": False}
+        phase_1_0 = {"value": initial_gates[gt]["phi"]+delta_iq+90, "increment": False}
+        ct_entry_0 = {"index": idx, "waveform": waveform_0, "phase0": phase_0_0, "phase1": phase_1_0}
+        ct.append(ct_entry_0)
+        idx += 1
+
+    waves = [{"index": 0, "awgChannel0": ["sigout0","sigout1"]}, {"index": 1, "awgChannel0": ["sigout0","sigout1"]}]
+    phases = [{"value": 0, "increment": True}, {"value": 90, "increment": True}, {"value": 180, "increment": True}, {"value": 270, "increment": True}, {"value": -90, "increment": True},  {"value": -180, "increment": True},{"value": -270, "increment": True}]
+
+    ct_idx = idx + 1
+    for i in range(n_phases):
+        ct.append({"index": ct_idx, "waveform": waves[0], "phase0": phases[i], "phase1": phases[i]})
+        ct_idx += 1
+    ct_idx = ct_idx + 1
+    for i in range(n_phases):
+        ct.append({"index": ct_idx, "waveform": waves[1], "phase0": phases[i], "phase1": phases[i]})
+        ct_idx += 1
+    ct_idx = ct_idx +1
+    for i in range(n_phases):
+        ct.append({"index": ct_idx, "phase0": phases[i], "phase1": phases[i]})
+        ct_idx += 1
+
+    if len(npoints_wait) > 0:
+        ct_idx = ct_idx +1
+        for n_t in npoints_wait:
+            ct.append({"index": ct_idx, "waveform": {"playZero": True, "length": n_t}, "phase0": {"value": 0,  "increment": True}, "phase1": {"value": 0,  "increment": True}})
+            ct_idx += 1
+
+    command_table  = {'$schema': 'https://json-schema.org/draft-04/schema#', 'header': {'version': '0.2'}, 'table': ct}
+    return command_table
+
+
 def make_waveform_placeholders(n_array):
     ##Input: n_array. List of lengths for each gate operation.
     idx = 0
@@ -213,6 +267,30 @@ def make_command_table_idxs(gt_seq, sample_rate):
     waits = {}
     phi_l = 0
     ct_idx_t = 21
+    for gt in gt_seq:
+        if gt[0] in {"x" , "y", "m"}:
+            phi_l, phi_a = compute_accumulated_phase(gt, phi_l)
+            ct_idx = get_ct_idx(phi_a, gt)
+        elif gt[0] == "t":
+            n_t = ceil(sample_rate*int(gt[1:len(gt)])*(1e-9)/48)*48
+            if n_t in waits:
+                ct_idx = waits[str(n_t)]
+            else:
+                waits[str(n_t)] = ct_idx_t
+                ct_idx = ct_idx_t
+                n_ts.append(n_t)
+                ct_idx_t += 1
+        else:
+            pass
+        ct_idxs.append(ct_idx)
+    return ct_idxs, n_ts
+
+def make_command_table_idxs_v2(n_gt0, n_t_t, gt_seq, sample_rate):
+    ct_idxs = []
+    n_ts = []
+    waits = {}
+    phi_l = 0
+    ct_idx_t = 21 + n_gt0 + n_t_t
     for gt in gt_seq:
         if gt[0] in {"x" , "y", "m"}:
             phi_l, phi_a = compute_accumulated_phase(gt, phi_l)
