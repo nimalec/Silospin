5882ce4f79cc0c6786a15d638b78568e6b72e012
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-06-30 13:04:49 -0400

    ...

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -296,18 +296,21 @@ class MultiQubitGST:
             waveforms.assign_waveform(slot = 1, wave1 = waveforms_tau_pi_2[str(awg_idx)])
             waveforms_qubits[str(awg_idx)] = waveforms
             seq_code[awg_idx] =  make_waveform_placeholders(n_array)
-            command_code[awg_idx] = ""
-            #command_code[awg_idx] = []
+            #command_code[awg_idx] = ""
+            command_code[awg_idx] = []
             for idx in range(len(ct_idxs_all)):
                 n_seq = ct_idxs_all[idx][awg_idx]
                 sequence = make_gateset_sequencer_fast_v2(idx, n_seq)
                 command_code[awg_idx] = command_code[awg_idx] + sequence
-                #command_code[awg_idx].append(sequence)
-            sequencer_code[awg_idx] =  seq_code[awg_idx] + command_code[awg_idx]
+                command_code[awg_idx].append(sequence)
+            #sequencer_code[awg_idx] =  seq_code[awg_idx] + command_code[awg_idx]
+
 
         self._sequencer_code = sequencer_code
+
         for awg_idx in self._awg_cores:
-            self._awg.load_sequence(sequencer_code[awg_idx], awg_idx=awg_idx)
+            print(command_code[awg_idx][len(command_code[awg_idx])])
+        #     self._awg.load_sequence(sequencer_code[awg_idx], awg_idx=awg_idx)
         #     self._awg._awgs["awg"+str(awg_idx+1)].write_to_waveform_memory(waveforms)
         #
         # self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
