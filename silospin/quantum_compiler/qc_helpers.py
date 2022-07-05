cad6d7fef4814b7a12330661c4406f425ead65c2
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-05 16:35:04 -0400

    gst

--- silospin/quantum_compiler/qc_helpers.py
+++ silospin/quantum_compiler/qc_helpers.py
@@ -397,6 +397,8 @@ def make_command_table_idxs_v4(gt_seqs, tau_pi_s, tau_pi_2_s):
                         ct_idx_list.append(22)
                         ct_idx_list.append(22)
                         ct_idx_list.append(22)
+                        print(tau_pi_2_s)
+                        print(int(gt[1:len(gt)]))
                     # elif int(gt[1:len(gt)]) == tau_pi_s:
                     #     ct_idx_list.append(23)
                     #     ct_idx_list.append(23)
@@ -419,7 +421,7 @@ def make_command_table_idxs_v4(gt_seqs, tau_pi_s, tau_pi_2_s):
                         ct_idx_list.append(ct_idx)
                         ct_idx_list.append(23)
                 elif gt[0] == "t":
-                    print(int(gt[1:len(gt)]))
+                    #print(int(gt[1:len(gt)]))
                     if int(gt[1:len(gt)]) == tau_pi_2_s:
                         ct_idx = 22
                     elif int(gt[1:len(gt)]) == tau_pi_s:
