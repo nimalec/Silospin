af00f78920032a4a4dbb4121274b9c591bfef881
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-07 12:56:39 -0400

    gst

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -428,8 +428,8 @@ class MultiQubitGST_v2:
              self._awg.set_osc_freq(osc_idx, self._qubit_parameters[idx]["mod_freq"])
              self._awg.set_sine(i_idx+1, osc_idx)
              self._awg.set_sine(q_idx+1, osc_idx)
-             self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[str(awg_idx)]["i_amp_pi"])
-             self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[str(awg_idx)]["q_amp_pi"])
+             self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[idx]["i_amp_pi"])
+             self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[idx]["q_amp_pi"])
              daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_table))
 
     # def run_program(self, awg_idxs=None):
