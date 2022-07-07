145f7b561bedf736b0713f32f29d71e5cba592a3
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-07 11:38:58 -0400

    gst

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -368,9 +368,9 @@ class MultiQubitGST_v2:
         for idx in qubits:
             qubit_lengths[idx]["pi"] = ceil(tau_pi_standard_new*1e9)
             qubit_lengths[idx]["pi_2"] = ceil(tau_pi_2_standard_new*1e9)
-            print(qubit_lengths[idx]["pi_2"])
             qubit_npoints[idx]["pi"] = ceil(self._sample_rate*self._qubit_parameters[idx]["tau_pi"]/32)*32
             qubit_npoints[idx]["pi_2"] = ceil(self._sample_rate*self._qubit_parameters[idx]["tau_pi_2"]/32)*32
+        print(qubit_lengths)
 
         self._waveforms = generate_waveforms(qubit_npoints, tau_pi_2_standard_idx, amp=1)
         self._gate_sequences =  quantum_protocol_parser(gst_file_path, qubit_lengths, qubit_set = {1,2,3,4})
