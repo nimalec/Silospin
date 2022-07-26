ecd8445b4dfee1a49d84052e0bad5683606d73ea
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-26 15:01:04 -0400

    awg

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -635,7 +635,7 @@ class MultiQubitGST_v4:
              ## Need to change up implementation ==> 1 ct per qubit for all lines.
              command_tables[idx] = generate_reduced_command_table_v4(npoints_pi_2_standard, npoints_pi_standard, arbZ=arbZ)
 
-        command_tables = generate_reduced_command_table_v4(npoints_pi_2_standard, npoints_pi_standard, arbZ=arbZ)
+        #command_tables = generate_reduced_command_table_v4(npoints_pi_2_standard, npoints_pi_standard, arbZ=arbZ)
         self._ct_idxs = ct_idxs_all
         self._command_tables = command_tables
 
