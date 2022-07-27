eb2f9fc4c1284e6b60ca8c71eb26542e8b85b1ae
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-27 09:57:40 -0400

    aw

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -822,17 +822,16 @@ class MultiQubitGST_v5:
              self._awg.set_sine(q_idx+1, osc_idx)
              self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[idx]["i_amp_pi"])
              self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[idx]["q_amp_pi"])
-             #daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_tables[0]))
              daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_tables))
-    #
-    # def run_program(self, awg_idxs=None):
-    #     if awg_idxs:
-    #         awg_idxs = awg_idxs
-    #     else:
-    #         awg_idxs = self._awg_idxs
-    #     for idx in awg_idxs:
-    #         self._awg._awgs["awg"+str(idx+1)].single(True)
-    #         self._awg._awgs["awg"+str(idx+1)].enable(True)
+
+    def run_program(self, awg_idxs=None):
+        if awg_idxs:
+            awg_idxs = awg_idxs
+        else:
+            awg_idxs = self._awg_idxs
+        for idx in awg_idxs:
+            self._awg._awgs["awg"+str(idx+1)].single(True)
+            self._awg._awgs["awg"+str(idx+1)].enable(True)
 
 class MultiQubitRamseyTypes:
     ##should generalize for all qubits ==> only change will be pulse type
