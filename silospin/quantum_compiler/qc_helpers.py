30a6ddd4e88b0fd4159c1aaa65f22520776506cd
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-07-29 10:28:43 -0400

    awg

--- silospin/quantum_compiler/qc_helpers.py
+++ silospin/quantum_compiler/qc_helpers.py
@@ -314,7 +314,7 @@ def generate_reduced_command_table_v4(n_pi_2, n_pi, arbZ=[]):
     # phases_0_I = [{"value": 0}, {"value": 90}, {"value": 180}, {"value": -90}]
     # phases_0_Q = [{"value": 90}, {"value": 180}, {"value": 270}, {"value": 0}]
     phases_0_I = [{"value": 0}, {"value": 0}, {"value": 0}, {"value": 0}]
-    phases_0_Q = [{"value": -90}, {"value": -90}, {"value": -90}, {"value": -90}]
+    phases_0_Q = [{"value": 90}, {"value": 90}, {"value": 90}, {"value": 90}]
 
     #Incremented phases
     phases_incr = [{"value": 0, "increment": True}, {"value": 90, "increment": True}, {"value": 180, "increment": True}, {"value": 270, "increment": True}, {"value": -90, "increment": True},  {"value": -180, "increment": True},{"value": -270, "increment": True}]
