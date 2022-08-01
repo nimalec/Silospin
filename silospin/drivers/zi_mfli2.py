f61941e85b9acca125d125b5b1aba9fe75bca39d
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-08-01 12:36:52 -0400

    li

--- silospin/drivers/zi_mfli2.py
+++ silospin/drivers/zi_mfli2.py
@@ -30,6 +30,7 @@ class MFLI2:
         self._sigouts = {0: {"on": 1, "enable":  1, "range": 1 , "amplitude": 1}}
 
         demod_index = 0
+        in_channel = 0  
         ##make list of settings to upload to daq
         self._lockin_settings = [
         ["/%s/sigins/%d/ac" % (device, in_channel), self._sigins[0]["ac"]],
