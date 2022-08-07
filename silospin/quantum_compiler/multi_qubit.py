1ac03fabe2e359852c3b9d7ffd7563432aaac8a4
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-08-07 16:53:45 -0400

    awg

--- silospin/quantum_compiler/multi_qubit.py
+++ silospin/quantum_compiler/multi_qubit.py
@@ -802,13 +802,15 @@ class MultiQubitGST_v5:
              i_idx = self._channel_idxs[str(idx)][0]
              q_idx = self._channel_idxs[str(idx)][1]
              osc_idx = self._channel_osc_idxs[str(idx)]
-             self._awg._hdawg.sigouts[i_idx].on(1)
-             self._awg._hdawg.sigouts[q_idx].on(1)
+             # self._awg._hdawg.sigouts[i_idx].on(1)
+             # self._awg._hdawg.sigouts[q_idx].on(1)
              self._awg.set_osc_freq(osc_idx, self._qubit_parameters[idx]["mod_freq"])
              self._awg.set_sine(i_idx+1, osc_idx)
              self._awg.set_sine(q_idx+1, osc_idx)
              self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[idx]["i_amp_pi"])
              self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[idx]["q_amp_pi"])
+             self._awg._hdawg.sigouts[i_idx].on(1)
+             self._awg._hdawg.sigouts[q_idx].on(1)
              daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_tables))
 
     def run_program(self, awg_idxs=None):
