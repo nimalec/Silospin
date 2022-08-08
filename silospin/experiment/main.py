d1edd5afbb63671fd65f15314bbe7a061c278d95
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-08-07 21:50:49 -0400

    experiment

--- silospin/experiment/main.py
+++ silospin/experiment/main.py
@@ -2,3 +2,5 @@ from silospin.experiment import setup_experiment
 
 setup_experiment.init()
 print(setup_experiment.awg_driver)
+print(setup_experiment.mfli_driver)
+print(setup_experiment.qubit_parameters)
