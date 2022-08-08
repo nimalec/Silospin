f1174baff885bba0d8c4e9d434b8644dfb0353aa
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-08-07 21:28:45 -0400

    awg

---
+++ silospin/experiment/main.py
@@ -0,0 +1,4 @@
+from silospin.experiment.setup_experiment import init
+
+init()
+print(awg_driver)
---
+++ silospin/experiment/setup_experiment.py
@@ -0,0 +1,5 @@
+from silospin.drivers.zi_hdawg_driver import HdawgDriver
+from silospin.drivers.zi_mfli2 import MfliDriver
+
+def init(awg_id = "dev8446"):
+    global awg_driver = HdawgDriver(awg_id)
