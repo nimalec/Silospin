59ed2b5d46aab15f1757f6d96af1c5bf7c5c5dd5
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-08-07 22:05:18 -0400

    exp

--- silospin/test_experiments/test_gst.py
+++ silospin/test_experiments/test_gst.py
@@ -2,6 +2,6 @@ from silospin.experiment import setup_experiment
 from silospin.quantum_compiler.multi_qubit import MultiQubitGST_v5
 
 def GST(gst_file):
-    setup_experiment .init()
+    setup_experiment.init()
     awg = setup_experiment.awg_driver
     gst = MultiQubitGST_v5(gst_file, awg)
