6acc74f2bcc66c737ee30d303163a03986c2f3c1
asigilli <88289715+asigilli@users.noreply.github.com>
2022-09-05 17:03:21 -0400

    Update homedac_box.py
    Added some comments on a bug + places where we need to check for errors. It will be important that we maintain 6 digit/20 bit precision so please check the strings you're sending to the dac box and make sure we aren't truncating/rounding the numbers. 
    
    We will also want to have some dictionary mapping channel numbers to names so that we can specify for example dac.sweep2d("p1", "b1", 1.025, 1.030, 50, 0.625, 0.660, 50). We will also need to save somewhere (probably in same dictionary) parameters for each channel. i.e. each channel should have a voltage multiplier (we will have voltage dividers in the filter boxes that we want to be able to account for), There should also be software limits for maximum and minimum voltage applies (i.e. you shouldn't be able to apply more than 0.8V on any given gate after taking into account multipliers, etc.).
    
    We still need to add in data collection for every step in the loop.
    
    So, we will need to
    - fix bug when generating 2d sweeps
    - check precision of voltage sent.
    - add in dictionary of parameters
    - add function in sweeper that checks input strings against dictionary entries and returns channel value for programming dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -10,7 +10,7 @@ class DacDriver:
         n_channels = 25
         self._channel_configuration = {}
         for i in range(1,n_channels):
-            if i < 10:
+            if i < 10: # I think you can make this a lot less clunky by specifying the length of the number using string formatting- equivalent to printf in python seems to be (print("%02d" % (number,)))
                 self._dac.query("CH 0"+str(i))
             else:
                 self._dac.query("CH "+str(i))
@@ -24,7 +24,7 @@ class DacDriver:
             self._dac.query("CH 0"+str(channel))
         else:
             self._dac.query("CH "+str(channel))
-        self._dac.query("VOLT "+str(voltage))
+        self._dac.query("VOLT "+str(voltage)) # We need to be able to specify voltage to 6 digits of precision over the range from -10 to 10. Most sweeps will be something like 4.0001V -> 4.0003V. I'm not sure what the precision is - can you check this?        
         voltage_str = self._dac.query("VOLT?")
         self._channel_configuration[channel] = float(voltage_str[0:3])
 
@@ -52,8 +52,8 @@ class DacDriver:
 
 
     def Sweep2D(self, channel_1, channel_2, start_v_1, end_v_1, start_v_2, end_v_2, n_points_1, n_points_2):
-        dVx = (end_v_1-start_v_1)/n_points_1
-        dVy = (end_v_2-start_v_2)/n_points_2
+        dVx = (end_v_1-start_v_1)/n_points_1 # I think this should be (n_points_1 - 1) - check your sweep, you seem to never hit end_v_1
+        dVy = (end_v_2-start_v_2)/n_points_2# I think this should be (n_points_2 - 1)
         #V_x, V_y = np.mgrid[start_v_1:end_v_1:dVx,start_v_2:end_v_2:dVy]
         vx = np.arange(start_v_1, end_v_1, dVx)
         vy = np.arange(start_v_2, end_v_2, dVy)
