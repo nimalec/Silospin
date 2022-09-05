997a4bd866c03a776e4af34f65fb30b6815ffb99
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-05 16:00:33 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -52,11 +52,11 @@ class DacDriver:
 
 
     def Sweep2D(self, channel_1, channel_2, start_v_1, end_v_1, start_v_2, end_v_2, n_points_1, n_points_2):
-        #dVx = (end_v_1-start_v_1)/n_points_1
-        #dVy = (end_v_2-start_v_2)/n_points_2
+        dVx = (end_v_1-start_v_1)/n_points_1
+        dVy = (end_v_2-start_v_2)/n_points_2
         #V_x, V_y = np.mgrid[start_v_1:end_v_1:dVx,start_v_2:end_v_2:dVy]
-        vx = np.linspace(start_v_1, end_v_1, n_points_1)
-        vy = np.linspace(start_v_2, end_v_2, n_points_2)
+        vx = np.arrange(start_v_1, end_v_1, dVx)
+        vy = np.arrange(start_v_2, end_v_2, dVy)
         V_x, V_y = np.meshgrid(vx, vy)
 
         (dim0, dim1) = np.shape(V_x)
