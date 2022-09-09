193800e72b27dc82d251881c5ade3ad9c45a3e74
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-09 14:37:20 -0400

    numeric

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -401,7 +401,7 @@ class MfliDaqModule:
 
     def continuous_numeric(self, time_constant=10e-3, acquisition_time=0.001, sample_rate=3000):
         #self._mfli.set_demods_settings("timeconstant", time_constant)
-        data = self.continuous_data_acquisition_time_domain(burst_duration, n_bursts = 1, signal_nodes = ["r"], sample_rate=sample_rate)
+        data = self.continuous_data_acquisition_time_domain(acquisition_time, n_bursts = 1, signal_nodes = ["r"], sample_rate=sample_rate)
         time.sleep(0.5)
         signal_path = f"/{self._dev_id}/demods/0/sample.r"
         val = np.mean(data[0][signal_path][0]['value'])
