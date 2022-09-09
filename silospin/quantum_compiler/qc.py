48246bc53fcde964c3cfb1877389b58097d82308
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-09 09:15:43 -0400

    awg

--- silospin/quantum_compiler/qc.py
+++ silospin/quantum_compiler/qc.py
@@ -289,7 +289,6 @@ class GateSetTomographyProgramPlunger:
         for idx in plunger_set:
             p_dict[idx[0]] = ceil(idx[1]*1e-9)
 
-
         gate_standard_lengths = {"pi_2": ceil(tau_pi_2_standard*1e-9), "pi": ceil(tau_pi_standard*1e-9), "p": p_dict}
         print(gate_standard_lengths)
 
