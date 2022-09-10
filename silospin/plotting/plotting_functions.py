48ef690829e746d59a68a72590e92c0a8ee0276c
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-10 12:27:47 -0400

    dac

--- silospin/plotting/plotting_functions.py
+++ silospin/plotting/plotting_functions.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt 
 def plot1DVoltageSweep(input_voltage, output_voltage, idx, channel_number):
     plt.figure(figsize=(5, 2.7))
     plt.plot(input_voltage[0:idx-1], output_voltage[0:idx-1], label='linear', color = "red")
