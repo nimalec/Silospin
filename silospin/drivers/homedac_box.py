524af594fc08d55e9fc789adfee91adecfd4af96
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-13 11:45:31 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -17,6 +17,7 @@ class DacDriver:
         #self._id_name = self._dac.query("*IDN?")
         #self._id_name = self._dac.query("*IDN?\n")
         self._dac.write("*IDN?\n")
+        time.sleep(1)
         self._dac.read_bytes(100)
 
         n_channels = 25
