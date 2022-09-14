86a8c6cc237ec64cb31be974dd6d81181dde1daf
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-14 11:21:02 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -6,9 +6,9 @@ class DacDriver:
     def __init__(self, dev_id = "ASRL3::INSTR"):
         rm = pyvisa.ResourceManager()
         self._dev_id = dev_id
-        self._dac = rm.open_resource(self._dev_id, baud_rate=250000)
-        print(self._dac.baud_rate)
-        #self._dac = rm.open_resource(self._dev_id, baud_rate=250000, read_termination='\n', write_termination='\n')
+        #self._dac = rm.open_resource(self._dev_id, baud_rate=250000)
+        #print(self._dac.baud_rate)
+        self._dac = rm.open_resource(self._dev_id, baud_rate=250000, read_termination='\r\n', write_termination='\r\n')
         #self._dac.baud_rate  = 250000
         #self._dac.set_visa_attribute("baud_rate", int(250000))
         # self._dac.set_visa_attribute("read_termination", "\n")
