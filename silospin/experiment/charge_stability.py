23590f9a037d3f2fc27510cc21a550ee16be2848
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-16 14:58:33 -0400

    dac

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -3,6 +3,7 @@ from silospin.drivers.zi_mfli_driver import MfliDaqModule as DaqModule
 from silospin.drivers.homedac_box import DacDriver
 from silospin.plotting.plotting_functions import plot1DVoltageSweep, plot2DVoltageSweep
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation 
 import numpy as np
 import time
 
