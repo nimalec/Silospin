f8e90496851099d18dc1eb8229284a41f738b60c
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-18 14:49:10 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -5,6 +5,7 @@ import time
 
 class DacDriver:
     def __init__(self, dev_id = "ASRL3::INSTR", verbose=0, init=True, termination_char = '\r\n', baud_rate=250000):
+        ##Resolve flush issue for DAC
         rm = pyvisa.ResourceManager()
         baud_rate = 250000
         self._dev_id = dev_id
@@ -25,33 +26,11 @@ class DacDriver:
 
     def set_channel_and_voltage(self, channel, voltage):
         self._dac.write("CH\s"+str(channel))
-        self._dac.write("VOLT\s"+str(voltage))
+        self._dac.write("VOLT\s"+"{:.9f}".format(voltage))
 
     def set_voltage(self, voltage):
-        self._dac.write("VOLT\s"+str(voltage))
+        self._dac.write("VOLT\s"+"{:.9f}".format(voltage))
 
-        # n_channels = 25
-        # self._channel_configuration = {}
-        # for i in range(1,n_channels):
-        #     #self._dac.query("CH "+str(i))
-        #     self._dac.write("CH "+str(i))
-        #     voltage = self._dac.query("VOLT?")
-        #     self._channel_configuration[i] = float(voltage[0:3])
-
-    # def set_voltage(self, channel, voltage):
-    #     #self._dac.query("CH "+str(channel))
-    #     #self._dac.query("VOLT "+str(voltage))
-    #     self._dac.write("CH "+str(channel))
-    #     self._dac.write("VOLT "+str(voltage))
-    #     voltage_str = self._dac.query("VOLT?")
-    #     self._channel_configuration[channel] = float(voltage_str[0:3])
-
-    # def get_voltage(self, channel):
-    #     self._dac.query("CH "+str(channel))
-    #     voltage_str = self._dac.query("VOLT?")
-    #     self._channel_configuration[channel] = float(voltage_str[0:3])
-    #     return self._channel_configuration[channel]
-    #
     # def Sweep1D(self, channel, start_v, end_v, npoints):
     #     #self._dac.query("CH "+str(channel))
     #     self._dac.write("CH "+str(channel))
