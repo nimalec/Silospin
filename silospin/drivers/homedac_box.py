bf90df705283137790d369533b40cd4eaf16353d
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-18 10:55:54 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -8,7 +8,7 @@ class DacDriver:
         rm = pyvisa.ResourceManager()
         baud_rate = 250000
         self._dev_id = dev_id
-        self._dac = rm.open_resource(self._dev_id, baud_rate=baud_rate, read_termination=termination_char, write_termination=termination_char, timeout=2500)
+        self._dac = rm.open_resource(self._dev_id, baud_rate=baud_rate, read_termination=termination_char, write_termination=termination_char)
         # self._dac.write("VERBOSE\s"+str(verbose)+"\r\n")
         # self._dac.write("VERBOSE\s"+str(verbose)+"\r\n")
         # self._dac.query("*IDN?\r\n")
