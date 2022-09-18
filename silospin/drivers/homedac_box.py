63d4ced7dfcb66bdc2276aba583259ce9a2f36ec
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-18 12:46:04 -0400

    dac

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -14,7 +14,7 @@ class DacDriver:
             self._dac.write("INIT")
         else:
             pass
-        time.sleep(2)
+        time.sleep(1)
         self._dac.query("*IDN?")
 
     def set_verbose(self, verbose):
