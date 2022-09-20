a1f1f80854c9fd80049d8be42d37eeab3c0e3b1c
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-20 13:29:46 -0400

    awg

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -470,16 +470,17 @@ class MfliDaqModule:
 
     def continuous_numeric(self):
         signal_path = f"/{self._dev_id}/demods/0/sample.r"
-        #self._mfli._daq_module.execute()
         data_read = {}
-        #for i in range(60):
+        i = 0
         while not self._daq_module.finished():
             data_read = self._daq_module.read(True)
             returned_signal_paths = [signal_path.lower() for signal_path in data_read.keys()]
             if signal_path.lower() in returned_signal_paths:
                 val = data_read[signal_path.lower()][0]["value"][0]
+                i += 1
             else:
                 pass
+        print(i)
         #data_read = self._daq_module.read(True)
         return data_read
 
