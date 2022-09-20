c76795ec99d2f3c76392335b1ccfaf26550f8644
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-20 13:04:03 -0400

    awg

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -471,9 +471,10 @@ class MfliDaqModule:
     def continuous_numeric(self):
         #self._mfli._daq_module.execute()
         data_read = {}
-        while len(data_read)<1:
+        for i in range(60)
+        #while len(data_read)<1:
             data_read = self._daq_module.read(True)
-        data_read = self._daq_module.read(True)
+        #data_read = self._daq_module.read(True)
         return data_read
 
         # while not self._daq_module.finished():
