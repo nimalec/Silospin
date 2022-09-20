627bbd0bf55f8dcc6108483283fab4dd6f568ec2
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-20 12:29:58 -0400

    daq

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -472,7 +472,7 @@ class MfliDaqModule:
         data[signal_path] = []
         self._daq_module.subscribe(signal_path)
         self._mfli._daq_module.execute()
-        data_read = self._daq_module.read(True)
+        # data_read = self._daq_module.read(True)
 
         # while not self._daq_module.finished():
         #     data_read = self._daq_module.read(True)
