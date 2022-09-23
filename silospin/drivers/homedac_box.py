263c977d3296d551b7f7d215b10732eb1e51b3e2
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-23 14:37:18 -0400

    dac new

--- silospin/drivers/homedac_box.py
+++ silospin/drivers/homedac_box.py
@@ -38,18 +38,18 @@ class DacDriver:
 class DacDriverSerial:
     def __init__(self, dev_id = 'COM3', verbose=0, init=True, termination_char = '\n', baud_rate=250000):
         self._dev_id = dev_id
-        self._dac = serial.Serial(self._dev_id, baudrate=baud_rate, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,timeout=1)
+        self._dac = serial.Serial(self._dev_id, baudrate=baud_rate, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS,timeout=1, write_timeout=2)
         time.sleep(1)
-        cmd_1 = '*IDN?\r\n'
+        cmd_1 = '*IDN?\n'
         self._dac.write(cmd_1.encode('utf-8'))
         time.sleep(1)
         outputstring_1 = ''
         outputstring_1 = self._dac.readline().decode('utf-8').strip()
         print(outputstring_1)
-        cmd_2 = 'VERBOSE\s'+str(int(verbose))+'\r\n'
+        cmd_2 = 'VERBOSE\s'+str(int(verbose))+'\n'
         self._dac.write(cmd_2.encode('utf-8'))
         time.sleep(1)
-        cmd_3 = 'INIT\r\n'
+        cmd_3 = 'INIT\n'
 
         if init == True:
             self._dac.write(cmd_3.encode('utf-8'))
@@ -58,13 +58,13 @@ class DacDriverSerial:
             pass
 
     def set_voltage(self, voltage):
-        self._dac.write(str('VOLT\s'+'{:.6f}'.format(voltage)+'\r\n').encode('utf-8'))
+        self._dac.write(str('VOLT\s'+'{:.6f}'.format(voltage)+'\n').encode('utf-8'))
 
     def set_channel(self, channel):
-        self._dac.write(str('CH\s'+str(int(channel))+'\r\n').encode('utf-8'))
+        self._dac.write(str('CH\s'+str(int(channel))+'\n').encode('utf-8'))
 
     def set_init(self):
-        self._dac.write('INIT\r\n'.encode('utf-8'))
+        self._dac.write('INIT\n'.encode('utf-8'))
 
     def set_verbose(self, verbose):
-        self._dac.write(str('VEROBSE\s'+str(int(verbose))+'\r\n').encode('utf-8'))
+        self._dac.write(str('VEROBSE\s'+str(int(verbose))+'\n').encode('utf-8'))
