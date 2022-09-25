cef24e251a66bbdd7eeeea6e9aa52b46ab3bdd1d
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-25 14:32:04 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -158,7 +158,7 @@ class ChargeStabilitySweepsSerial:
                 ax.set_ylabel("Measured output [V]")
                 if i == npoints-2:
                     v_outer.append(v_inner)
-                    print(v_outer)
+                    v_inner.clear()
                     if len(v_outer) == n_fr:
                         plotter.pause()
                     else:
