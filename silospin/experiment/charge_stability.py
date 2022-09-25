50531953ab674a8054b7c04e091dc36d6921e5df
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-25 14:24:27 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -156,6 +156,7 @@ class ChargeStabilitySweepsSerial:
                 ax.plot(v_array[0:len(v_inner)], v_inner)
                 ax.set_xlabel("Applied barrier voltage [V]")
                 ax.set_ylabel("Measured output [V]")
+                print(v_inner)
                 if i == npoints-2:
                     v_outer.append(v_inner)
                     #v_inner.clear()
