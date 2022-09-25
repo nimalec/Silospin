d2f21d09145fa24e360d84d01f49a95b4358f45e
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-25 19:35:11 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -213,7 +213,7 @@ class ChargeStabilitySweepsSerial:
                     V_out = output_voltages_f_t.reshape([npoints[0], npoints[1]])
                     z_min = np.min(output_voltages_f_t)
                     z_max = np.min(output_voltages_f_t)
-                    print(V_out)  
+                    print(V_out)
                     c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
                     cbar = fig.colorbar(c, ax=ax0)
                     cbar.set_label('Output Voltage [V]', rotation=270)
@@ -222,9 +222,9 @@ class ChargeStabilitySweepsSerial:
                 else:
                     pass
 
-            plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=0.001, repeat=True)
+            plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=0.001, repeat=False)
+            return plotter, (V_x, V_y)
             plt.show()
-            return (V_x, V_y)
 
 
         # # self._dac.set_channel(channel)
