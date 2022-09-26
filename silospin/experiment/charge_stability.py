96fa56598b8d0f194e038cd78c676b5a7d4ffb39
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-25 20:52:03 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -197,6 +197,7 @@ class ChargeStabilitySweepsSerial:
         V_y_f = V_y.flatten()
         global output_voltages_f
         output_voltages_f = output_voltages.flatten()
+        V_outs = []
 
         if plot == True:
             fig, ax = plt.subplots()
@@ -210,9 +211,11 @@ class ChargeStabilitySweepsSerial:
                     v_meas = self._mfli.get_sample_r()
                     output_voltages_new = v_meas*output_voltages_f
                     V_out = output_voltages_new.reshape([npoints[0], npoints[1]])
+                    V_outs.append(V_out)
                     z_min = np.min(output_voltages_new)
                     z_max = np.min(output_voltages_new)
-                    c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
+                    #c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
+                    c = ax.pcolor(V_x, V_y, V_outs[i], cmap='RdBu', vmin=z_min, vmax=z_max)
                     ax.set_xlabel("Left barrier voltage [V]")
                     ax.set_ylabel("Right barrier voltage [V]")
                     global v_out_temp
@@ -226,10 +229,11 @@ class ChargeStabilitySweepsSerial:
                     v_meas = self._mfli.get_sample_r()
                     v_out_temp[i] = v_meas
                     V_out_temp = v_out_temp.reshape([npoints[0], npoints[1]])
-                    print(V_out_temp)
                     z_min = np.min(v_out_temp)
                     z_max = np.min(v_out_temp)
-                    c = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', vmin=z_min, vmax=z_max)
+                    V_outs.append(V_out)
+                    #c = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', vmin=z_min, vmax=z_max)
+                    c = ax.pcolor(V_x, V_y, V_outs[i], cmap='RdBu', vmin=z_min, vmax=z_max)
                     ax.set_xlabel("Left barrier voltage [V]")
                     ax.set_ylabel("Right barrier voltage [V]")
             plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=0.001, repeat=False)
