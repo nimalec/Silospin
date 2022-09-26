6223fe477cb3a831696a09c0679c0b6cb7e67946
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-25 20:15:20 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -222,9 +222,10 @@ class ChargeStabilitySweepsSerial:
                     self._dac.set_channel(channels[1])
                     self._dac.set_voltage(V_y_f[i])
                     v_meas = self._mfli.get_sample_r()
-                    output_voltages_new[i] = v_meas
-                    z_min = np.min(output_voltages_new)
-                    z_max = np.min(output_voltages_new)
+                    output_voltages_temp = output_voltages_new
+                    output_voltages_temp[i] = v_meas
+                    z_min = np.min(output_voltages_temp)
+                    z_max = np.min(output_voltages_temp)
                     c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
                     ax.set_xlabel("Left barrier voltage [V]")
                     ax.set_ylabel("Right barrier voltage [V]")
