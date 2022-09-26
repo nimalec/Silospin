edc570b855d609e9005375502c4e96dd18ec843b
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-26 12:39:36 -0400

    awg

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -227,80 +227,10 @@ class ChargeStabilitySweepsSerial:
                     V_out_temp = v_out_temp.reshape([npoints[0], npoints[1]])
                     z_min = np.min(v_out_temp)
                     z_max = np.min(v_out_temp)
-                    #cplot = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', vmin=z_min, vmax=z_max)
-                    #cplot = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', norm=plt.Normalize(0, 1e-6))
                     cplot = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', norm=plt.Normalize(0, 1e-7))
                     ax.set_xlabel("Left barrier voltage [V]")
                     ax.set_ylabel("Right barrier voltage [V]")
-                return cplot
+                return cplot,
             plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=0.00001, repeat=False)
-            return plotter, (V_x, V_y)
+            return plotter, (V_x, V_y, V_out)
             plt.show()
-
-    # def sweep2D(self, channel_1, channel_2, start_v_1, end_v_1, start_v_2, end_v_2, n_points_1, n_points_2):
-    #     ##Note: need to add external loopss
-    #     vx = np.linspace(start_v_1, end_v_1, n_points_1)
-    #     vy = np.linspace(start_v_2, end_v_2, n_points_2)
-    #     V_x, V_y = np.meshgrid(vx, vy)
-    #     output_voltages = np.ones((n_points_1, n_points_2))
-    #     V_x_f = V_x.flatten()
-    #     V_y_f = V_y.flatten()
-    #     output_voltages_f = output_voltages.flatten()
-    #
-    #     fig, ax = plt.subplots()
-    #     def plot2Dtrace(i):
-    #         if i == 0:
-    #             self._dac.set_channel(channel_1)
-    #             self._dac.set_voltage(V_x_f[i])
-    #             self._dac.set_channel(channel_2)
-    #             self._dac.set_voltage(V_y_f[i])
-    #             v_meas = self._mfli.get_sample_r()
-    #             ax.clear()
-    #             output_voltages_f_t = v_meas*output_voltages_f
-    #             V_out = output_voltages_f_t.reshape([n_points_1, n_points_2])
-    #             z_min = np.min(output_voltages_f_t)
-    #             z_max = np.max(output_voltages_f_t)
-    #             c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
-    #             cbar = fig.colorbar(c, ax=ax0)
-    #             cbar.set_label('Output Voltage [V]', rotation=270)
-    #             ax.set_xlabel("Left barrier voltage [V]")
-    #             ax.set_ylabel("Right barrier voltage [V]")
-    #         else:
-    #             if V_x_f[i] == V_x_f[i-1]:
-    #                 self._dac.set_channel(channel_2)
-    #                 self._dac.set_voltage(V_y_f[i])
-    #                 v_meas = self._mfli.get_sample_r()
-    #                 ax.clear()
-    #                 output_voltages_f_t[i] = v_meas
-    #                 V_out = output_voltages_f_t.reshape([n_points_1, n_points_2])
-    #                 z_min = np.min(output_voltages_f_t)
-    #                 z_max = np.max(output_voltages_f_t)
-    #                 c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
-    #                 cbar = fig.colorbar(c, ax=ax0)
-    #             elif V_y_f[i] == V_y_f[i-1]:
-    #                 self._dac.set_channel(channel_1)
-    #                 self._dac.set_voltage(V_x_f[i])
-    #                 v_meas = self._mfli.get_sample_r()
-    #                 ax.clear()
-    #                 output_voltages_f_t[i] = v_meas
-    #                 V_out = output_voltages_f_t.reshape([n_points_1, n_points_2])
-    #                 z_min = np.min(output_voltages_f_t)
-    #                 z_max = np.max(output_voltages_f_t)
-    #                 c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
-    #                 cbar = fig.colorbar(c, ax=ax0)
-    #             else:
-    #                 self._dac.set_channel(channel_1)
-    #                 self._dac.set_voltage(V_x_f[i])
-    #                 self._dac.set_channel(channel_2)
-    #                 self._dac.set_voltage(V_y_f[i])
-    #                 v_meas = self._mfli.get_sample_r()
-    #                 ax.clear()
-    #                 output_voltages_f_t[i] = v_meas
-    #                 V_out = output_voltages_f_t.reshape([n_points_1, n_points_2])
-    #                 z_min = np.min(output_voltages_f)
-    #                 z_max = np.max(output_voltages_f)
-    #                 c = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', vmin=z_min, vmax=z_max)
-    #                 cbar = fig.colorbar(c, ax=ax0)
-    #     plotter = FuncAnimation(fig, plot2Dtrace, frames=n_points_1*n_points_2, interval=0.001, repeat=False)
-    #     return plotter
-    #     plt.show()
