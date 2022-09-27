9c0fd5e2e2b3171e14a33929bc25d3c44ba5923b
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-27 15:44:06 -0400

    dac

--- silospin/experiment/charge_stability.py
+++ silospin/experiment/charge_stability.py
@@ -161,72 +161,22 @@ class ChargeStabilitySweepsSerial:
                 v_outer.append(v_inner)
             return v_mean.append(np.mean(np.array(v_outer),axis=0))
 
-    def sweep2DFrameAverage(self, channels, v_range, npoints, n_fr = 1, plot = True):
-        v_x = np.linspace(v_range[0][0], v_range[0][1], npoints[0])
-        v_y = np.linspace(v_range[1][0], v_range[1][1], npoints[1])
-        V_x, V_y = np.meshgrid(v_x, v_y)
-        output_voltages = np.ones((npoints[0], npoints[1]))
-        V_x_f = V_x.flatten()
-        V_y_f = V_y.flatten()
-        global output_voltages_f
-        output_voltages_f = output_voltages.flatten()
-
-        if plot == True:
-            fig, ax = plt.subplots()
-            def plot2Dtrace(i):
-                ax.clear()
-                if i == 0:
-                    self._dac.set_channel(channels[0])
-                    self._dac.set_voltage(V_x_f[i])
-                    self._dac.set_channel(channels[1])
-                    self._dac.set_voltage(V_y_f[i])
-                    v_meas = self._mfli.get_sample_r()
-                    output_voltages_new = v_meas*output_voltages_f
-                    V_out = output_voltages_new.reshape([npoints[0], npoints[1]])
-                    z_min = np.min(output_voltages_new)
-                    z_max = np.min(output_voltages_new)
-                    cplot = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', norm=plt.Normalize(0, 1e-6))
-                    ax.set_xlabel("Left barrier voltage [V]")
-                    ax.set_ylabel("Right barrier voltage [V]")
-                    global v_out_temp
-                    v_out_temp  = output_voltages_new
-                else:
-                    self._dac.set_channel(channels[0])
-                    self._dac.set_voltage(V_x_f[i])
-                    self._dac.set_channel(channels[1])
-                    self._dac.set_voltage(V_y_f[i])
-                    v_meas = self._mfli.get_sample_r()
-                    v_out_temp[i] = v_meas
-                    V_out_temp = v_out_temp.reshape([npoints[0], npoints[1]])
-                    z_min = np.min(v_out_temp)
-                    z_max = np.min(v_out_temp)
-                    cplot = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', norm=plt.Normalize(0,1e-6))
-                    ax.set_xlabel("Left barrier voltage [V]")
-                    ax.set_ylabel("Right barrier voltage [V]")
-                return cplot,
-            plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=0.00001, repeat=False)
-            return plotter, (V_x, V_y, v_out_temp)
-            plt.show()
-
-    def sweep2DFrameAverage_v2(self, channels, v_range, npoints, n_r = 10, n_fr = 1, plot = True):
+    def sweep2D(self, channels, v_range, npoints, n_r = 10, n_fr = 1, plot = True):
         ##Defines initial parameters (input voltages)
         v_x = np.linspace(v_range[0][0], v_range[0][1], npoints[0])
         v_y = np.linspace(v_range[1][0], v_range[1][1], npoints[1])
         V_x, V_y = np.meshgrid(v_x, v_y)
-        ##Defines output voltages ==> make a flattened array
         output_voltages = np.ones((npoints[0], npoints[1]))
         V_x_f = V_x.flatten()
         V_y_f = V_y.flatten()
         output_voltages_f = output_voltages.flatten()
         V_outs = []
         V_mean = []
-        v_mean = None
 
         if plot == True:
             fig, ax = plt.subplots()
             def plot2Dtrace(i):
                 ax.clear()
-                ## if at the first index ==> set both voltages and initialize array
                 if i == 0:
                     self._dac.set_channel(channels[0])
                     self._dac.set_voltage(V_x_f[i])
@@ -237,11 +187,16 @@ class ChargeStabilitySweepsSerial:
                     V_out = output_voltages_new.reshape([npoints[0], npoints[1]])
                     z_min = np.min(output_voltages_new)
                     z_max = np.min(output_voltages_new)
-                    cplot = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', norm=plt.Normalize(0, 1e-6))
-                    ax.set_xlabel("Left barrier voltage [V]")
-                    ax.set_ylabel("Right barrier voltage [V]")
-                    global v_out_temp
-                    v_out_temp  = output_voltages_new
+
+                    if i%n_r == 0:
+                    ## Add condition for plotting (e.g.: if i%n_r == 0: generate a ploot )
+                        cplot = ax.pcolor(V_x, V_y, V_out, cmap='RdBu', norm=plt.Normalize(0, 1e-6))
+                        ax.set_xlabel("Left barrier voltage [V]")
+                        ax.set_ylabel("Right barrier voltage [V]")
+                        global v_out_temp
+                        v_out_temp  = output_voltages_new
+                    else:
+                        pass
                 else:
                     self._dac.set_channel(channels[0])
                     self._dac.set_voltage(V_x_f[i])
@@ -255,18 +210,16 @@ class ChargeStabilitySweepsSerial:
                     cplot = ax.pcolor(V_x, V_y, V_out_temp, cmap='RdBu', norm=plt.Normalize(0,1e-6))
                     ax.set_xlabel("Left barrier voltage [V]")
                     ax.set_ylabel("Right barrier voltage [V]")
-
                     if i == npoints[0]*npoints[1]-1:
                         V_outs.append(V_out_temp)
                         if len(V_outs) == n_fr:
                             plotter.pause()
                             V_mean.append(np.mean(np.array(V_outs),axis=0))
-                            #v_mean = np.mean(np.array(V_outs),axis=0)
                         else:
                             pass
                     else:
                         pass
                 return cplot,
             plotter = FuncAnimation(fig, plot2Dtrace, frames=npoints[0]*npoints[1], interval=1, repeat=True)
-            return plotter, (V_x, V_y, V_mean)
+            return (V_x, V_y, V_mean), plotter
             plt.show()
