0ffb8c25ec620c65073393d526dd0a53aa265710
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-29 12:53:57 -0400

    awg

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -496,14 +496,17 @@ class MfliDaqModule:
         ##val = None
         #print(self._daq_module.finished())
         #data_read = self._daq_module.read(True)
-        while not self._daq_module.finished():
-            data_read = self._daq_module.read(True)
-            returned_signal_paths = [signal_path.lower() for signal_path in data_read.keys()]
-            if signal_path.lower() in returned_signal_paths:
-               val = data_read[signal_path.lower()][0]["value"][0]
-               return val
-            else:
-                 pass
+        data_read = self._daq_module.read(True)
+        return data_read 
+
+        # while not self._daq_module.finished():
+        #     data_read = self._daq_module.read(True)
+        #     returned_signal_paths = [signal_path.lower() for signal_path in data_read.keys()]
+        #     if signal_path.lower() in returned_signal_paths:
+        #        val = data_read[signal_path.lower()][0]["value"][0]
+        #        return val
+        #     else:
+        #          pass
         #return val
 
         # while not self._daq_module.finished():
