26f5da8cc81d1a4e805f3ae7660c402b89455241
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-09-29 13:31:12 -0400

    awg

--- silospin/drivers/zi_mfli_driver.py
+++ silospin/drivers/zi_mfli_driver.py
@@ -473,8 +473,8 @@ class MfliDaqModule:
         self.set_trigger_setting("type", 0)
         self.set_grid_setting("mode", 4)
         signal_path = f"/{self._dev_id}/demods/0/sample.r"
-        self._daq_module.set("count", 100)
-        self._daq_module.set("grid/cols", 4)
+        self._daq_module.set("count", 0)
+        self._daq_module.set("grid/cols",1000)
         self._daq_module.set("holdoff/time", 0)
         self._daq_module.set("refreshrate", 500)
         self._daq_module.subscribe(signal_path)
