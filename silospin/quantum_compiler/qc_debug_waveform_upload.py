a0eeb9a3f1c66172a333499735781eb05e4c9135
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-05 15:37:04 -0400

    awg

--- silospin/quantum_compiler/qc_debug_waveform_upload.py
+++ silospin/quantum_compiler/qc_debug_waveform_upload.py
@@ -2,7 +2,7 @@ from zhinst.toolkit import Waveforms
 import numpy as np
 
 class TestWaveformUpload:
-    def __init__(self, hdawg=awg):
+    def __init__(self, hdawg):
         waveforms = Waveforms()
         waveforms.assign_waveform(slot=0, wave1=np.ones(256))
         for idx in rnage(0,4):
