9982c365a4eb5ebfcbd327923f6b604797346c36
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-05 14:53:23 -0400

    awg

--- silospin/quantum_compiler/qc.py
+++ silospin/quantum_compiler/qc.py
@@ -155,9 +155,8 @@ class GateSetTomographyProgram:
 
         for idx in qubits:
              self._awg.load_sequence(sequencer_code[idx], awg_idx=idx)
-             time.sleep(0.4)
+             time.sleep(1)
              self._awg._awgs["awg"+str(idx+1)].write_to_waveform_memory(waveforms_awg[idx])
-             print(self._awg._awgs["awg"+str(idx+1)])
 
         self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
         self._channel_osc_idxs = {"0": 1, "1": 5, "2": 9, "3": 13}
