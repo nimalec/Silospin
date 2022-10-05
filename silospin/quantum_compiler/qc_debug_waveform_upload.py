087a4e0d1367dc7fc20ff14fd00c1c7c098cbc16
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-05 16:01:28 -0400

    awg

--- silospin/quantum_compiler/qc_debug_waveform_upload.py
+++ silospin/quantum_compiler/qc_debug_waveform_upload.py
@@ -1,11 +1,13 @@
+from silospin.drivers.zi_hdawg_driver import HdawgDriver as hdawg
 from zhinst.toolkit import Waveforms
 from zhinst.toolkit import Session
 import numpy as np
 
 class TestWaveformUpload:
-    def __init__(self, dev_id = "dev8446"):
-        session = Session('localhost')
-        device = session.connect_device(dev_id)
+    def __init__(self, dev_id="dev8446"):
+        device = hdawg(dev_id)
+        #session = Session('localhost')
+        #evice = session.connect_device(dev_id)
         waveforms = Waveforms()
         waveforms.assign_waveform(slot=0, wave1=np.ones(256))
         for idx in range(0,4):
