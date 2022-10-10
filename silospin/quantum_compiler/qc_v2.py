b22ed194175900e42436a654f4a19a3bee1cec4a
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-10 14:47:11 -0400

    gst

--- silospin/quantum_compiler/qc_v2.py
+++ silospin/quantum_compiler/qc_v2.py
@@ -239,7 +239,7 @@ class GateSetTomographyProgramPlunger:
 
         for idx in range(0,3):
             self._awg.load_sequence(self._sequencer_code[idx-1], awg_idx=idx)
-            self._awg._awgs["awg"+str(idx+1)].write_to_waveform_memory(waveforms_awg[idx])
+            self._awg._awgs["awg"+str(idx+1)].write_to_waveform_memory(waveforms_awg[idx-1])
 
     #     self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
     #     self._channel_osc_idxs = {"0": 1, "1": 5, "2": 9, "3": 13}
