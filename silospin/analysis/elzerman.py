8070e052738c5dc656f592fcbefe45fa1818dee4
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-09 21:39:45 -0400

    gst

--- silospin/analysis/elzerman.py
+++ silospin/analysis/elzerman.py
@@ -4,9 +4,9 @@ def digitize_current(i_trace, t_start, t_end, threshold, sample_rate):
     n_start = ceil(t_start*sample_rate)
     n_end = ceil(t_end*sample_rate)
     i_trace_sub_set =  i_trace[n_start:n_end]
-    i_max = np.max(i_trace_sub_set)
-    if  i_max >= threshold:
-        dig_val = 1
-    else:
+    count = np.count_nonzero(i_trace_sub_set  > threshold)
+    if count == 0:
         dig_val = 0
-    return dig_val 
+    else:
+        dig_val = 1 
+    return dig_val
