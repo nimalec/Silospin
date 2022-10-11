4e6ba9b0036b3dc004e9e3172221157db52d659d
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-11 19:17:04 -0400

    awg

--- silospin/quantum_compiler/qc_v2.py
+++ silospin/quantum_compiler/qc_v2.py
@@ -235,26 +235,26 @@ class GateSetTomographyProgramPlunger:
             self._awg.load_sequence(self._sequencer_code[idx+1], awg_idx=idx)
             self._awg._awgs["awg"+str(idx+1 )].write_to_waveform_memory(waveforms_awg[idx+1])
 
-    #     self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
-    #     self._channel_osc_idxs = {"0": 1, "1": 5, "2": 9, "3": 13}
+        self._channel_idxs = {"0": [0,1], "1": [2,3], "2": [4,5], "3": [6,7]}
+        self._channel_osc_idxs = {"0": 1, "1": 5, "2": 9, "3": 13}
 
-    #     daq = self._awg._daq
-    #     dev = self._awg._connection_settings["hdawg_id"]
+        daq = self._awg._daq
+        dev = self._awg._connection_settings["hdawg_id"]
 
-    #     # ##9. Modify to only set sine waves for modulation cores
-    #     rf_cores_2 = [0,1,2]
-    #     for idx in rf_cores_2:
-    #           i_idx = self._channel_idxs[str(idx)][0]
-    #           q_idx = self._channel_idxs[str(idx)][1]
-    #           osc_idx = self._channel_osc_idxs[str(idx)]
-    #           self._awg.set_osc_freq(osc_idx, self._qubit_parameters[idx]["mod_freq"])
-    #           self._awg.set_sine(i_idx+1, osc_idx)
-    #           self._awg.set_sine(q_idx+1, osc_idx)
-    #           self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[idx]["i_amp_pi"])
-    #           self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[idx]["q_amp_pi"])
-    #           self._awg._hdawg.sigouts[i_idx].on(1)
-    #           self._awg._hdawg.sigouts[q_idx].on(1)
-    #           daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_tables['rf']))
+        # ##9. Modify to only set sine waves for modulation cores
+        rf_cores_2 = [0,1,2]
+        for idx in rf_cores_2:
+              i_idx = self._channel_idxs[str(idx)][0]
+              q_idx = self._channel_idxs[str(idx)][1]
+              osc_idx = self._channel_osc_idxs[str(idx)]
+              self._awg.set_osc_freq(osc_idx, self._qubit_parameters[idx]["mod_freq"])
+              self._awg.set_sine(i_idx+1, osc_idx)
+              self._awg.set_sine(q_idx+1, osc_idx)
+              self._awg.set_out_amp(i_idx+1, 1, self._qubit_parameters[idx]["i_amp_pi"])
+              self._awg.set_out_amp(q_idx+1, 2, self._qubit_parameters[idx]["q_amp_pi"])
+              self._awg._hdawg.sigouts[i_idx].on(1)
+              self._awg._hdawg.sigouts[q_idx].on(1)
+              daq.setVector(f"/{dev}/awgs/{idx}/commandtable/data", json.dumps(self._command_tables['rf']))
 
     #      p_idx = 3
     #      i_idx = self._channel_idxs[str(p_idx)][0]
