3d6b8562718ed36b358be1676077ffa8994b9bf4
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-12 15:16:47 -0400

    dac

--- silospin/drivers/zi_hdawg_driver.py
+++ silospin/drivers/zi_hdawg_driver.py
@@ -101,7 +101,7 @@ class HdawgDriver:
             ch_2 = self._channel_mapping[idx]['ch']['index'][1]
             if self._channel_mapping[idx]['rf'] == 1:
                 self.set_modulation_mode(ch_1, 1)
-                self.set_modulation_mode(ch_ 2, 2)
+                self.set_modulation_mode(ch_2, 2)
             elif self._channel_mapping[idx]['rf'] == 0:
                 self.set_modulation_mode(ch_1, 0)
                 self.set_modulation_mode(ch_2, 0)
