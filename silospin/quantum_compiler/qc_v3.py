7d6f38b71285e549d379004e60c96e0c557401d7
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-21 17:53:05 -0400

    awg

--- silospin/quantum_compiler/qc_v3.py
+++ silospin/quantum_compiler/qc_v3.py
@@ -260,11 +260,11 @@ class GateSetTomographyProgramPlunger_V3:
         self._awg._hdawg.sigouts[7].on(1)
         daq.setVector(f"/{dev}/awgs/{p_idx}/commandtable/data", json.dumps(self._command_tables['plunger']))
 
-    # def run_program(self, awg_idxs=None):
-    #     if awg_idxs:
-    #         awg_idxs = awg_idxs
-    #     else:
-    #         awg_idxs = [0,1,2,3]
-    #     for idx in awg_idxs:
-    #         self._awg._awgs["awg"+str(idx+1)].single(True)
-    #         self._awg._awgs["awg"+str(idx+1)].enable(True)
+    def run_program(self, awg_idxs=None):
+        if awg_idxs:
+            awg_idxs = awg_idxs
+        else:
+            awg_idxs = [0,1,2,3]
+        for idx in awg_idxs:
+            self._awg._awgs["awg"+str(idx+1)].single(True)
+            self._awg._awgs["awg"+str(idx+1)].enable(True)
