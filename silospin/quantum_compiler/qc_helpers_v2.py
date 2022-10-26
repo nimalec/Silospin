1049c15e5ada82da84236cab4c9a3b55a1b4cc95
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-25 22:13:28 -0400

    awg

--- silospin/quantum_compiler/qc_helpers_v2.py
+++ silospin/quantum_compiler/qc_helpers_v2.py
@@ -55,8 +55,10 @@ def make_gateset_sequencer(n_seq):
 def make_gate_npoints(gate_parameters, sample_rate):
     gate_npoints = {"rf": {}, "plunger": {}}
     for idx in gate_parameters["rf"]:
-        n_pi = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi"]*1e-9/48)*48
-        n_pi_2 = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi_2"]*1e-9/48)*48
+        #n_pi = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi"]*1e-9/48)*48
+        #n_pi_2 = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi_2"]*1e-9/48)*48
+        n_pi = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi"]*1e-9)
+        n_pi_2 = ceil(sample_rate*gate_parameters["rf"][idx]["tau_pi_2"]*1e-9)
         gate_npoints["rf"][idx] = {"pi": n_pi, "pi_2": n_pi_2}
     for idx in gate_parameters["p"]:
         n_p = ceil(sample_rate*gate_parameters["p"][idx]["tau"]*1e-9/48)*48
