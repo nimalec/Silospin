bb4d759106a7a888e5d4cac6a9b147a26f7ebab3
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-26 12:34:18 -0400

    awg

--- silospin/quantum_compiler/qc_helpers_v3.py
+++ silospin/quantum_compiler/qc_helpers_v3.py
@@ -965,9 +965,9 @@ def make_waveform_placeholders_plungers(n_array):
     line_6 = "assignWaveIndex(placeholder("+str(n_array[5])+"),"+ "placeholder("+str(n_array[5])+"),"+"5"+");\n"
     line_7 = "assignWaveIndex(placeholder("+str(n_array[6])+"),"+"6"+");\n"
     #line_8 = "assignWaveIndex(placeholder("+str(n_array[7])+"),"+"7"+");\n"
-    line_8 = "assignWaveIndex(placeholder("+str(n_array[7])+"),"+ "placeholder("+str(n_array[7])+"),"+"6"+");\n"
-    line_9 = "assignWaveIndex(placeholder("+str(n_array[4])+"),"+ "placeholder("+str(n_array[5])+"),"+"7"+");\n"
-    line_10 = "assignWaveIndex(placeholder("+str(n_array[6])+"),"+ "placeholder("+str(n_array[7])+"),"+"8"+");\n"
+    line_8 = "assignWaveIndex(placeholder("+str(n_array[7])+"),"+ "placeholder("+str(n_array[7])+"),"+"7"+");\n"
+    line_9 = "assignWaveIndex(placeholder("+str(n_array[4])+"),"+ "placeholder("+str(n_array[5])+"),"+"8"+");\n"
+    line_10 = "assignWaveIndex(placeholder("+str(n_array[6])+"),"+ "placeholder("+str(n_array[7])+"),"+"9"+");\n"
     sequence_code = line_1+line_2+line_3+line_4+line_5+line_6+line_7+line_8+line_9+line_10
     return sequence_code
 
