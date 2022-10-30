aa87c1c237a28694c9f7b9006e87916ead5fb8c0
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-10-30 10:57:56 -0400

    awg

--- silospin/quantum_compiler/qc_v3.py
+++ silospin/quantum_compiler/qc_v3.py
@@ -469,7 +469,7 @@ class GateSetTomographyProgramPlunger_V4:
                 seq = make_gateset_sequencer_hard_trigger_v2(n_seq, n_inner, trig_channel=True)
             sequence += seq
         command_code[idx] = command_code[idx] + sequence
-        sequencer_code[idx] = seq_code[idx] + command_code[idx]
+        sequencer_code[idx] = seq_code[idx] + command_code[idx] + "}" 
 
         self._sequencer_code = sequencer_code
         for idx in range(0,4):
