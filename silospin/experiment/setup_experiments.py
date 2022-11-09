0cda168043db43876298e1f9c8931557b366868f
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-11-09 12:01:59 -0800

    experiment

--- silospin/experiment/setup_experiments.py
+++ silospin/experiment/setup_experiments.py
@@ -1,14 +1,15 @@
 from silospin.quantum_compiler.quantum_compiler import GateSetTomographyQuantumCompiler
 from silospin.experiment.setup_experiment_helpers import initialize_drivers
+from silospin.experiment import setup_experiment_helpers
 from silospin.quantum_compiler.quantum_compiler_helpers import make_gate_parameters
 
 class GSTExperiment:
     def __init__(self, gst_file, n_inner=1, n_outer=1, added_padding=0, drivers_initialized=False, descriptor="gst_experiment"):
         if drivers_initialized:
-            self._instrument_drivers = {'awgs': {0: initialize_drivers.awg_driver_1}, "mflis": {0: initialize_drivers.mfli_driver_1, 1: initialize_drivers.mfli_driver_2}, 'dac': {0: initialize_drivers.dac_box_1}}
+            self._instrument_drivers = {'awgs': {0: setup_experiment_helpers.awg_driver_1}, "mflis": {0: setup_experiment_helpers.mfli_driver_1, 1: setup_experiment_helpers.mfli_driver_2}, 'dac': {0: setup_experiment_helpers.dac_box_1}}
         else:
             initialize_drivers()
-            self._instrument_drivers = {'awgs': {0: initialize_drivers.awg_driver_1}, "mflis": {0: initialize_drivers.mfli_driver_1, 1: initialize_drivers.mfli_driver_2}, 'dac': {0: initialize_drivers.dac_box_1}}
+            self._instrument_drivers = {'awgs': {0: setup_experiment_helpers.awg_driver_1}, "mflis": {0: setup_experiment_helpers.mfli_driver_1, 1: setup_experiment_helpers.mfli_driver_2}, 'dac': {0: setup_experiment_helpers.dac_box_1}}
         self._gst_file = gst_file
         #experiment_parameters =  get_parameters_file(parameters_file_path)
         #self._gate_parameters = experiment_parameters['qubits']
@@ -21,7 +22,7 @@ class GSTExperiment:
         plunger_lengths = {7: 5, 8: 5}
         plunger_amp =  {7:  1, 8: 1}
         gate_param = make_gate_parameters(tau_pi, tau_pi_2, i_amp, q_amp, mod_freq, plunger_lengths, plunger_amp)
-        self._gst_program = GateSetTomographyProgramPlunger_V4(self._gst_file, self._gate_parameters, self._instrument_drivers['awgs'][0], n_inner, n_outer)
+        self._gst_program = GateSetTomographyQuantumCompiler(self._gst_file, self._gate_parameters, self._instrument_drivers['awgs'][0], n_inner, n_outer)
 
     def run_program(self):
         self._gst_program.run_program()
