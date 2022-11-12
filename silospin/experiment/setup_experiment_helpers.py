67bd3d58c2adf3429ce356f926b332585d745ccc
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-11-12 16:18:50 -0500

    experiment

--- silospin/experiment/setup_experiment_helpers.py
+++ silospin/experiment/setup_experiment_helpers.py
@@ -3,7 +3,7 @@ from silospin.drivers.zi_mfli_driver import MfliDriver
 from silospin.drivers.homedac_box import DacDriverSerial
 import pickle
 
-def initialize_drivers(awgs={0: 'dev8446', 1: 'dev8485'}, lockins={0: 'dev5759', 1: 'dev5761', 2: 'dev6573'}):
+def initialize_drivers(awgs={0: 'dev8446', 1: 'dev8485'}, lockins={0: 'dev5759', 1: 'dev5761'}):
     global awg_driver_1
     global mfli_driver_1
     global mfli_driver_2
@@ -12,7 +12,6 @@ def initialize_drivers(awgs={0: 'dev8446', 1: 'dev8485'}, lockins={0: 'dev5759',
     awg_driver_2 = HdawgDriver(awgs[1])
     mfli_driver_1 = MfliDriver(lockins[0])
     mfli_driver_2 = MfliDriver(lockins[1])
-    mfli_driver_3 = MfliDriver(lockins[2])
 
 def pickle_qubit_parameters(parameters_dict, parameters_file_path):
     with open(parameters_file_path, 'wb') as handle:
