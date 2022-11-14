5c5acf1e58f67a339be9e14c4535508d1349904a
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-11-13 22:48:34 -0500

    dac

--- silospin/experiment/charge_stability_v3.py
+++ silospin/experiment/charge_stability_v3.py
@@ -9,40 +9,120 @@ import numpy as np
 import time
 
 class ChargeStabilitySweepsSerial:
-    def __init__(self, dac_id="COM3", mfli_id= "dev5759", excluded_zurich_devices= ["dev8446", "dev5761"], filter_tc=10e-3):
-
-        ##Modify here to account for multiple instruments
-        self._mfli = MfliDriverChargeStability(excluded_devices = excluded_zurich_devices, timeconstant=filter_tc)
+    def __init__(self, dac_id="COM3", filter_tc=10e-3):
+        self._mflis = {0: MfliDriverChargeStability(excluded_devices=["dev5761", "dev8446", "dev8485", "dev6573"], timeconstant=filter_tc), 1: MfliDriverChargeStability(excluded_devices=["dev5759", "dev8446", "dev8485", "dev6573"], timeconstant=filter_tc)}
         self._dac = DacDriverSerial(dac_id)
 
-    def sweep1D(self, channel, start_v, end_v, npoints, n_r = 10, n_fr = 1, plot = True):
-        self._dac.set_channel(channel)
-        V_out_all_1 = []
+    def sweep1D(self, channel, start_v, end_v, npoints, lockins = {1, 2}, n_r = 10, n_fr = 1, plot = True):
+        ##Lockin 1 ==> "dev5759"
+        ##Lockin 2 ==> "dev5761"
         v_in_array = np.linspace(start_v,end_v,npoints)
-        if plot == True:
-            fig_1 = plt.figure()
-            ax_1 = fig_1.add_subplot(111)
-            for j in range(n_fr):
-                V_out_1 = []
-                for i in range(len(v_in_array)):
-                    self._dac.set_voltage(v_in_array[i])
-                    V_meas_1 = self._mfli.get_sample_r()
-                    V_out_1.append(V_meas_1)
-                    if i%n_r == 0:
-                        ax_1.plot(v_in_array[0:len(V_out_1)], V_out_1)
-                        ax_1.set_xlabel('Applied barrier voltage [V]')
-                        ax_1.set_ylabel('Measured output [V]')
-                        fig_1.canvas.draw()
-                        plt.show(block=False)
-                V_out_all_1.append(V_out_1)
+        self._dac.set_channel(channel)
 
+        if {1,2} == lockins:
+            V_out_all_1 = []
+            V_out_all_2 = []
+            if plot == True:
+                fig_1 = plt.figure()
+                ax_1 = fig_1.add_subplot(111)
+                fig_2 = plt.figure()
+                ax_2 = fig_2.add_subplot(111)
+                for j in range(n_fr):
+                    V_out_1 = []
+                    V_out_1 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[0].get_sample_r()
+                        V_meas_2 = self._mflis[1].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                        V_out_2.append(V_meas_2)
+                        if i%n_r == 0:
+                            ax_1.plot(v_in_array[0:len(V_out_1)], V_out_1)
+                            ax_1.set_xlabel('Applied barrier voltage [V]')
+                            ax_1.set_ylabel('Measured output on lock-in 1 [V]')
+                            fig_1.canvas.draw()
+
+                            ax_2.plot(v_in_array[0:len(V_out_2)], V_out_2)
+                            ax_2.set_xlabel('Applied barrier voltage [V]')
+                            ax_2.set_ylabel('Measured output on lock-in 2 [V]')
+                            fig_2.canvas.draw()
+
+                            plt.show(block=False)
+                    V_out_all_2.append(V_out_1)
+
+            else:
+                for j in range(n_fr):
+                    V_out_1 = []
+                    V_out_2 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[0].get_sample_r()
+                        V_meas_2 = self._mflis[1].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                        V_out_2.append(V_meas_2)
+                V_out_all_1.append(V_out_1)
+                V_out_all_2.append(V_out_2)
+                return_value = (v_in_array,np.mean(np.array(V_out_all_1),axis=0)), np.mean(np.array(V_out_all_2)),axis=0))
+
+        elif {1} == lockins:
+            V_out_all_1 = []
+            if plot == True:
+                fig_1 = plt.figure()
+                ax_1 = fig_1.add_subplot(111)
+                for j in range(n_fr):
+                    V_out_1 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[0].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                        if i%n_r == 0:
+                            ax_1.plot(v_in_array[0:len(V_out_1)], V_out_1)
+                            ax_1.set_xlabel('Applied barrier voltage [V]')
+                            ax_1.set_ylabel('Measured output [V]')
+                            fig_1.canvas.draw()
+                            plt.show(block=False)
+                    V_out_all_1.append(V_out_1)
+
+            else:
+                for j in range(n_fr):
+                    V_out_1 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[0].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                V_out_all_1.append(V_out_1)
+                return_value = (v_in_array,np.mean(np.array(V_out_all_1),axis=0)))
+
+        elif {2} == lockins:
+            V_out_all_1 = []
+            v_in_array = np.linspace(start_v,end_v,npoints)
+            if plot == True:
+                fig_1 = plt.figure()
+                ax_1 = fig_1.add_subplot(111)
+                for j in range(n_fr):
+                    V_out_1 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[1].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                        if i%n_r == 0:
+                            ax_1.plot(v_in_array[0:len(V_out_1)], V_out_1)
+                            ax_1.set_xlabel('Applied barrier voltage [V]')
+                            ax_1.set_ylabel('Measured output [V]')
+                            fig_1.canvas.draw()
+                            plt.show(block=False)
+                    V_out_all_1.append(V_out_1)
+
+            else:
+                for j in range(n_fr):
+                    V_out_1 = []
+                    for i in range(len(v_in_array)):
+                        self._dac.set_voltage(v_in_array[i])
+                        V_meas_1 = self._mflis[1].get_sample_r()
+                        V_out_1.append(V_meas_1)
+                V_out_all_1.append(V_out_1)
+                return_value = (v_in_array,np.mean(np.array(V_out_all_1),axis=0)))
         else:
-            for j in range(n_fr):
-                V_out_1 = []
-                for i in range(len(v_in_array)):
-                    self._dac.set_voltage(v_in_array[i])
-                    V_meas_1 = self._mfli.get_sample_r()
-                    V_out_1.append(V_meas_1)
-            V_out_all_1.append(V_out_1)
-            print(len(V_out_all_1))
-        return (v_in_array, np.mean(np.array(V_out_all_1),axis=0))
+            pass
+
+        return return_value
