bda4bf2be8c71066ad39122e47983ed153e07166
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-11-15 10:27:34 -0500

    dac

---
+++ silospin/experiment/sweeper_functions.py
@@ -0,0 +1,118 @@
+from silospin.drivers.set_val import *
+from silospin.drivers.homedac_box import DacDriverSerialServer
+from silospin.drivers.zi_mfli_driver import MfliDriverChargeStability
+
+import numpy as np
+
+def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, plot = True, lockins = {1,2,3}, filter_tc=10e-3, dac_mapping_file_path = 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac.pickle'):
+    dac_server = DacDriverSerialServer()
+    self._mflis = {0: MfliDriverChargeStability(excluded_devices=["dev5761", "dev8446", "dev8485", "dev6573"], timeconstant=filter_tc), 1: MfliDriverChargeStability(excluded_devices=["dev5759", "dev8446", "dev8485", "dev6573"], 2: MfliDriverChargeStability(excluded_devices=["dev5759", "dev8446", "dev8485", "dev5761"], timeconstant=filter_tc)}
+    gates = {"B1", "B2", "B3", "B4", "B5", "P1", "P2",  "P3", "P4", "L1", "L2",  "M1", "M2",  "R1", "R2",  "BS1", "BS2", "TS", "MS", "Source1", "Drain1", "Source2", "Drain2", "Source3", "Drain3"}
+    dac_dict = unpickle_qubit_parameters(dac_mapping_file_path)
+    channel_mapping = dac_dict["channel_mapping"]
+
+    if parameter == "channel_voltage_set" or parameter == "gates_voltages_set":
+        v_in_arrays = []
+        for i in range(len(start_value)):
+            v_in_arrays.append(np.linspace(start_value[i][1], end_value[i][1], npoints))
+    elif parameter in all_gates or parameter == "channel_voltage":
+        v_in_array = np.linspace(start_value, end_value, npoints)
+    else:
+        pass
+    lockin_configs = {1: {1,2,3}, 2: {1,2}, 3: {2,3}, 4: {1}, 5: {2}, 6: {3}}
+
+    if lockins == lockin_configs[1]:
+        V_out_all_1 = []
+        V_out_all_2 = []
+        V_out_all_3 = []
+
+        if plot == True:
+            fig_1 = plt.figure()
+            ax_1 = fig_1.add_subplot(111)
+            fig_2 = plt.figure()
+            ax_2 = fig_2.add_subplot(111)
+            fig_3 = plt.figure()
+            ax_3 = fig_2.add_subplot(111)
+
+            for i in n_fr:
+                V_out_1 = []
+                V_out_2 = []
+                V_out_3 = []
+
+                for j in range(npoints):
+                    if parameter == "channel_voltage_set" or parameter == "gates_voltages_set":
+                        for k in range(len(v_in_arrays)):
+                            ## For case where a set of voltage ranges per channel is provided
+
+
+                    else:
+                        set_val(parameter, v_in_array[j], channel_mapping, dac_server)
+                        V_out_1.append(self._mflis[0].get_sample_r())
+                        V_out_2.append(self._mflis[1].get_sample_r())
+                        V_out_3.append(self._mflis[2].get_sample_r())
+
+                        if j%n_r == 0:
+                            ax_1.plot(v_in_array[0:len(V_out_1)], V_out_1)
+                            ax_1.set_xlabel('Applied barrier voltage [V]')
+                            ax_1.set_ylabel('Measured output on lock-in 1 [V]')
+                            fig_1.canvas.draw()
+
+                            ax_2.plot(v_in_array[0:len(V_out_2)], V_out_2)
+                            ax_2.set_xlabel('Applied barrier voltage [V]')
+                            ax_2.set_ylabel('Measured output on lock-in 2 [V]')
+                            fig_2.canvas.draw()
+
+                            ax_3.plot(v_in_array[0:len(V_out_3)], V_out_3)
+                            ax_3.set_xlabel('Applied barrier voltage [V]')
+                            ax_3.set_ylabel('Measured output on lock-in 3 [V]')
+                            fig_3.canvas.draw()
+
+                            plt.show(block=False)
+
+    elif lockins in {lockin_configs[2], lockin_configs[3]}:
+        V_out_all_1 = []
+        V_out_all_2 = []
+
+    elif lockins in {lockin_configs[4], lockin_configs[5], lockin_configs[6]}:
+        V_out_all_1 = []
+
+
+    elif lockins == {1,2} or lockins == {2,3}:
+
+    elif lockins == {1} or lockins == {2} or lockins == {3}:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def do1DSweep(parameter, start_value, end_value, lockins = {1,2,3}, client_address = ):
