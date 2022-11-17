d0b13e3e254f66e79f1e4f78447feaedcef9b95c
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-11-17 14:30:43 -0500

    sweep

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -261,7 +261,7 @@ def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_
                          ax1.set_ylabel(parameter2+" gate voltage [V]")
                          fig1.canvas.draw()
                          cbar1 = fig1.colorbar(img1, ax=ax1, extend='both')
-                         cbar1.set_label('Demodulated voltage from lock-in 1[V]', rotation=270, labelpad=-40)
+                         cbar1.set_label('Demodulated voltage from lock-in 1[V]', rotation=270, labelpad=40)
                          plt.show(block=False)
                          #plt.xlim([-start_value1, end_value1])
                          #plt.ylim([-start_value2, end_value2])
