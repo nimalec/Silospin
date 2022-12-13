8bea7913b8855712feccdd4011fc0fed417e238c
nimlaec <47722154+nimalec@users.noreply.github.com>
2022-12-13 15:45:30 -0500

    awg

--- silospin/quantum_compiler/quantum_compiler_io_v2.py
+++ silospin/quantum_compiler/quantum_compiler_io_v2.py
@@ -238,6 +238,7 @@ def gst_file_parser_v3(file_path, qubit_lengths, arbgate_picklefile_location = '
                     length_set.append(qubit_length)
 
                 elif item.find('*') != -1:
+                    print(item)
                     gt_label_idx = item.find('*') + 1
                     gt_label = item[gt_label_idx]
                     gt_parameters = arb_gate_dict['parameters']
