1d7a0dbe2c588c9123cad7039ba778c8e0356fff
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-06 12:30:57 -0500

    dac

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -195,7 +195,6 @@ class GateSetTomographyQuantumCompiler:
                 #    arbitrary_z[awg_idx][core_idx], take this in
                 ## Input args for command table function: arbitrary_z[awg_idx][core_idx] (arb Zs), arbitrary_waveforms, std pulse lengths, pulse lengths
                     self._command_tables[awg_idx][core_idx] = make_rf_command_table_v2(n_std, arbitrary_z, plunger_set_npoints_tups, awg_idx, core_idx)
-                    print(self._command_tables[awg_idx][core_idx])
                 else:
                     ## Call on DC CT function
                     self._command_tables[awg_idx][core_idx] = {}
