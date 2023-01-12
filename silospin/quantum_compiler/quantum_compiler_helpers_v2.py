45f430e7a173b1f8cd66075ec37ea1f924b5dcd3
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-11 20:51:01 -0500

    qc_v2

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -679,18 +679,25 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
 
                 # pi/2 gate
                 elif gt in pi_2_gt_set:
-                    for tup in p_intersect:
-                        if tup[1] == 'p':
-                            tau_p = self._gate_lengths['plunger'][tup[0]]
-                            tau_pi_2 = self._gate_lengths['rf'][rf_idx]['pi_2']
-                            #Plunger frame
-                            if tau_p > tau_pi:
-                                gt_str = gt+'_p_fr'
-                            #Pi/2 frame
+                    if len(p_intersect) != 0:
+                        for tup in p_intersect:
+                            if tup[1] == 'p':
+                                tau_p = self._gate_lengths['plunger'][tup[0]]
+                                tau_pi_2 = self._gate_lengths['rf'][rf_idx]['pi_2']
+                                if len(pi_intersect) == 0 and tau_p > tau_pi_2:
+                                    gt_str = gt+'_p_fr'
+                                elif len(pi_intersect) == 0 and tau_p < tau_pi_2:
+                                    gt_str = gt+'_pi2_fr'
+                                elif len(pi_intersect) != 0 and tau_p < tau_pi:
+                                    gt_str = gt+'_pi_fr'
+                                else:
+                                    pass
                             else:
                                 gt_str = gt+'_pi2_fr'
-                        else:
-                            gt_str = gt+'_pi2_fr'
+                    elif len(pi_intersect) != 0:
+                        gt_str = gt+'_pi_fr'
+                    else:
+                        gt_str = gt+'_pi2_fr'
                     ct_idxs[awg_idx][core_idx].append(initial_gates[gt_str])
 
 # #                 ## Gates with pi/2 length
