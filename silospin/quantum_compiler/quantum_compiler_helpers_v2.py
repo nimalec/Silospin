12b01bd07d69339a14491a19460130336ecc67e5
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-16 12:45:59 -0500

    dac

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -672,9 +672,11 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
             p_intersect_tups = set([(seq, dc_gate_sequence[seq][idx]) for seq in dc_gate_sequence])
 
 
+
             if idx == 0:
                 # pi gate
                 if gt in pi_gt_set:
+                    print(gt)
                     if len(p_intersect) != 0:
                         for tup in p_intersect_tups:
                             if tup[1] == 'p':
@@ -715,7 +717,7 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
                     else:
                         gt_str = gt+'_pi2_fr'
                     ct_idxs[awg_idx][core_idx].append(initial_gates[gt_str])
-    print(ct_idxs)
+
                 # pi gate
                 # elif gt in pi_gt_set:
                 #     if len(p_intersect) != 0:
