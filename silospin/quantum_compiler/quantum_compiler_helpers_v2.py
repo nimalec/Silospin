856ff160f644c995a7d9e59fad488ed158fc04da
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-17 17:40:09 -0500

    ct_idxs

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -804,9 +804,10 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
                     ct_idxs[awg_idx][core_idx].append(ct_idx_g_a)
                 else:
                     pass
-                    ##Throw an error here...
+            ## Other RF gates         
+            else:
+                pass
 
-    print(ct_idxs)
     return arbgate_counter
             ##Case: gates following the first one
             # else:
