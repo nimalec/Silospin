c19efc95a00d18e65a94a3d9e710a7205be8f851
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-17 12:39:59 -0500

    awg

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -783,9 +783,10 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
 
                 elif gt.find('*') != -1:
                     if gt[gt.find('*')+1] in arbgate_dict.keys():
-                        arb_gate_counter[awg_idx][cored_idx] += 1
-                        ## Check if  X, Y, or otther gate ... 
-                        ct_idx_g_a  = 58 + N_z + N_p + arb_gate_counter[awg_idx][core_idx]
+                        print(gt)
+                        #arb_gate_counter[awg_idx][cored_idx] += 1
+                        ## Check if  X, Y, or other gate ...
+                        #ct_idx_g_a  = 58 + N_z + N_p + arb_gate_counter[awg_idx][core_idx]
 
                 #     if gt[gt.find('*')+1] in arbgate_dict.keys():
                 #         arb_gate_counter[awg_idx][cored_idx] += 1 ##Should return arb_gate_counter at the output
