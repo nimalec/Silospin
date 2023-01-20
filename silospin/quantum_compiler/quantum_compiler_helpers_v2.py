9808178d7bd54b97dd0835ef72005f45911c42d7
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-20 13:01:44 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -915,7 +915,8 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
     #         rf_gates_other = set([rf_gate_sequence[j][idx] for j in rf_diff_idxs])
     #         pi_2_intersect = rf_gates_other.intersection(pi_2_gt_set)
     #         pi_intersect = rf_gates_other.intersection(pi_gt_set)
-    #         p_intersect = set([dc_gate_sequence[seq][idx] for seq in dc_gate_sequence]).intersection({'p'})
+            p_intersect = set([dc_gate_sequence[seq][idx] for seq in dc_gate_sequence]).intersection({'p'})
+            print(p_intersect)
     #         p_intersect_tups = set([(seq, dc_gate_sequence[seq][idx]) for seq in dc_gate_sequence])
 
     return ct_idxs, arbgate_counter
