5a9f00980d6a3054981ef0323c3ba8686c9aeae1
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-21 23:07:31 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -1027,7 +1027,9 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
                         else:
                             itr += 1
                     ct_idxs[awg_idx][core_idx].append(ct_idx_p)
+
                 print(dc_idx, ct_idxs[awg_idx][core_idx])
+                print(ct_idx_p1_pi, ct_idx_p2_pi)
 
             elif gt == 'z0z':
                 ct_idxs[awg_idx][core_idx].append(ct_idx_z0z)
