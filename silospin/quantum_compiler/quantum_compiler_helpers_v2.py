a25bce4710df13ce8cfae54dc8ba2bd4acec79fe
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-22 11:55:54 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -927,6 +927,26 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
             pi_2_intersect = rf_gates_other.intersection(pi_2_gt_set)
             pi_intersect = rf_gates_other.intersection(pi_gt_set)
 
+
+
+
+            itr = 0
+            p_dc_intersect = {}
+            for j in dc_gate_sequence.keys():
+                gt_dc = dc_gate_sequence[j][idx]
+                if gt_dc == 'p':
+                    itr = 0
+                    for item in plunger_tup_lengths:
+                        if j == item[0]:
+                            p_dc_intersect[itr] = item[1]
+
+                        else:
+                            itr += 1
+                else:
+                    pass
+            p_dc_diff_max_idx = max(p_dc_intersect, key=p_dc_intersect.get)
+
+
             ## Edge cases:
             ##1. single plunger, no other dc or rf gates ==> no pulse centering (use (p_i)_{p_i})
             ##2. single plunger, no pi/pi_2 RF gates or plunger gates, but arbitrary gates or delays present ==> no pulse centering (use (p_i)_{p_i})
@@ -998,28 +1018,28 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
 
                 ##Case 4
                 elif len(pi_intersect) != 0:
-                    itr = 0
-                    p_pi_intersect = {}
-                    for j in dc_gate_sequence.keys():
-                        gt_dc = dc_gate_sequence[j][idx]
-                        if gt_dc == 'p':
-                            itr = 0
-                            for item in plunger_tup_lengths:
-                                if j == item[0]:
-                                    p_pi_intersect[itr] = item[1]
-
-                                else:
-                                    itr += 1
-                        else:
-                            pass
-                    p_diff_max_idx = max(p_pi_intersect, key=p_pi_intersect.get)
-
+                    # itr = 0
+                    # p_pi_intersect = {}
+                    # for j in dc_gate_sequence.keys():
+                    #     gt_dc = dc_gate_sequence[j][idx]
+                    #     if gt_dc == 'p':
+                    #         itr = 0
+                    #         for item in plunger_tup_lengths:
+                    #             if j == item[0]:
+                    #                 p_pi_intersect[itr] = item[1]
+                    #
+                    #             else:
+                    #                 itr += 1
+                    #     else:
+                    #         pass
+                    # p_diff_max_idx = max(p_pi_intersect, key=p_pi_intersect.get)
 
                     for item in plunger_tup_lengths:
                         if dc_idx == item[0]:
                             ##Work in std p frame
                             ##Note: instead of tau_p_gt ,
-                            if p_pi_intersect[p_diff_max_idx] > taus_std[1]:
+                            #if p_pi_intersect[p_diff_max_idx] > taus_std[1]:
+                            if p_dc_intersect[p_dc_diff_max_idx] > taus_std[1]:
                                 if dc_idx%2 != 0:
                                     ct_idx_p = p_std_idx
                                 else:
@@ -1034,6 +1054,7 @@ def make_command_table_indices_v3(gt_seqs, channel_map, awg_core_split, arb_gate
                         else:
                             itr += 1
                     ct_idxs[awg_idx][core_idx].append(ct_idx_p)
+                    print(ct_idxs[awg_idx][core_idx])
 
                 ##Case 5: working in  pi/2 frame
                 # elif len(pi_2_intersect) != 0 and len(pi_intersect) == 0:
