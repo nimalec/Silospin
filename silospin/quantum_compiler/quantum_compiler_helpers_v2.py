bcfeb7992f2f676daf2b9b805529b647ba0f8e23
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-24 17:52:07 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -1465,6 +1465,7 @@ def make_command_table_indices_v4(gt_seqs, channel_map, awg_core_split, arb_gate
                     pass
 
     ##Start here
+    print(dc_gate_sequence[11])
     print(list(dc_gate_sequence.keys())[0])
     n_gates = len(gate_sequence[list(dc_gate_sequence.keys())[0]])
     check_dc_p_channels = {}
