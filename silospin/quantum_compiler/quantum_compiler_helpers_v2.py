5336a7add625d2d8c80e73d67fd9e941968e6879
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-24 13:54:12 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -1524,8 +1524,6 @@ def make_command_table_indices_v4(gt_seqs, channel_map, awg_core_split, arb_gate
                     else:
                         ct_idx_p = itr + N_p
                     ct_idxs[awg_idx][core_idx].append(ct_idx_p)
-                    print(ct_idxs[awg_idx][core_idx])
-
 
 
             #     #Case 3
@@ -1671,6 +1669,7 @@ def make_command_table_indices_v4(gt_seqs, channel_map, awg_core_split, arb_gate
             #     pass
             #
             # ##Throw an error
+    print(ct_idxs)
     return ct_idxs, arbgate_counter
 
 
