9286f076fe5264e59feff6bbc61f0846ee3f2cb8
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-26 13:12:39 -0500

    seq

--- silospin/quantum_compiler/quantum_compiler_helpers_v2.py
+++ silospin/quantum_compiler/quantum_compiler_helpers_v2.py
@@ -2472,6 +2472,24 @@ def make_waveform_placeholders(n_array):
         idx+=1
     return sequence_code
 
+
+def make_waveform_placeholders_v2(waveform_lengths):
+    '''
+    Generates sequencer code for waveform placeholders on HDAWG FPGAs.
+
+    Parameters:
+                n_array (list): list of the number of points used for each waveform. Note, that each element of n_array must be at least 48 and an integer multiple of 16.
+    Returns:
+       sequence_code (str): sequencer code for waveform placeholders.
+    '''
+    idx = 0
+    sequence_code = ""
+    for wave_idx in waveform_lengths:
+        line = "assignWaveIndex(placeholder("+str(waveform_lengths[wave_idx][0])+"),"+"placeholder("+str(waveform_lengths[wave_idx][1])+"),"+str(wave_idx)+");\n"
+        sequence_code = sequence_code + line
+    print(sequence_code)
+    return sequence_code
+
 def make_waveform_placeholders_plungers(n_array):
     '''
     Generates sequencer code for plunger gate waveform placeholders on HDAWG FPGAs.
@@ -2535,7 +2553,7 @@ def make_gateset_sequencer_hard_trigger_v2(n_seq, n_av, trig_channel=True):
     command_code = ""
     for n in n_seq:
         #idx_str = str(n)
-        idx_str = str(n_seq[n])  
+        idx_str = str(n_seq[n])
         line = "executeTableEntry("+idx_str+");\n"
         command_code = command_code + line
 
