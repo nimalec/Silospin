0842465d6895f0be183fb7eda9d4be2c04857b1e
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-25 23:07:24 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -364,8 +364,7 @@ class GateSetTomographyQuantumCompiler:
                             for itr in arb_waveforms[line]:
                                  gate_tup = arb_waveforms[line][itr]
                                  if gate_tup[0][0] == 't':
-                                     wave_2 = evaluate_arb_waveform(gate_tup[1])
-                                     print(wave_2)
+                                     wave_2 = evaluate_arb_waveform(gate_tup[1]) 
                                      waveforms.assign_waveform(slot = wave_idx, wave1 = np.zeros(len(wave_2)), wave2 = wave_2)
                                      wave_idx += 1
 
