8be29a90899a866c3c57652979322126082c29e3
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-26 17:00:59 -0500

    dac

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -461,12 +461,12 @@ class GateSetTomographyQuantumCompiler:
 
 
 
-        for awg_idx in self._channel_mapping:
-            for core_idx in self._channel_mapping[awg_idx]:
-                daq = self._awgs[awg_idx]._daq
-                device_id = self._awgs[awg_idx]._connection_settings["hdawg_id"]
-                daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/elf/data", elf)
-                daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/commandtable/data", json.dumps(self._command_tables[awg_idx][core_idx]))
+        # for awg_idx in self._channel_mapping:
+        #     for core_idx in self._channel_mapping[awg_idx]:
+        #         daq = self._awgs[awg_idx]._daq
+        #         device_id = self._awgs[awg_idx]._connection_settings["hdawg_id"]
+        #         daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/elf/data", elf)
+        #         daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/commandtable/data", json.dumps(self._command_tables[awg_idx][core_idx]))
 
         # for awg_idx in self._channel_mapping:
         #     for core_idx in self._channel_mapping[awg_idx]:
@@ -477,9 +477,9 @@ class GateSetTomographyQuantumCompiler:
         #             daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
         #
 
-        for awg_idx in self._channel_mapping:
-            for core_idx in self._channel_mapping[awg_idx]:
-                self._awgs[awg_idx]._awgs["awg"+str(core_idx)].write_to_waveform_memory(waveforms_awg[awg_idx][core_idx])
+        # for awg_idx in self._channel_mapping:
+        #     for core_idx in self._channel_mapping[awg_idx]:
+        #         self._awgs[awg_idx]._awgs["awg"+str(core_idx)].write_to_waveform_memory(waveforms_awg[awg_idx][core_idx])
     #         self._awg._awgs["awg"+str(idx+1 )].write_to_waveform_memory(waveforms_awg[idx+1])
     #
     #     for idx in rf_cores:
