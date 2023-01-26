22059f21944ff7936978a0f956f352c9c2b0eda1
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-25 22:33:49 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -324,12 +324,10 @@ class GateSetTomographyQuantumCompiler:
                     ## 3.[(p1)_1, (p2)_1]...[(p1)_N, (p2)_N]
                     for i in plunger_idxs:
                     #    print('p'+str(channel_idxs_core[0])+'_p'+str(i)+'fr','p'+str(channel_idxs_core[1])+'_p'+str(i)+'fr')
-                        print('p'+str(channel_idxs_core[0])+'_p'+str(i)+'fr')
-                        print('p'+str(channel_idxs_core[1])+'_p'+str(i)+'fr')
                         wave_1 = np.array(self._waveforms[awg_idx][core_idx]['p'+str(channel_idxs_core[0])+'_p'+str(i)+'fr'])
                         wave_2 = np.array(self._waveforms[awg_idx][core_idx]['p'+str(channel_idxs_core[1])+'_p'+str(i)+'fr'])
-                        print('p'+str(channel_idxs_core[0])+'_p'+str(i)+'fr', len(wave_1))
-                        print('p'+str(channel_idxs_core[1])+'_p'+str(i)+'fr', len(wave_2))
+                        # print('p'+str(channel_idxs_core[0])+'_p'+str(i)+'fr', len(wave_1))
+                        # print('p'+str(channel_idxs_core[1])+'_p'+str(i)+'fr', len(wave_2))
                     #     waveforms.assign_waveform(slot = wave_idx, wave1 = wave_1, wave2 = wave_2)
                     #     wave_idx += 1
                     # ## 4. [(p1)_pi, 0]
