0d4a78b6489528d35eb753ce96ff46d12044e67e
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-01-31 12:27:47 -0500

    dac

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -27,8 +27,6 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
     dac_server = DacDriverSerialServer()
     #mflis = {0: MfliDriverChargeStability(dev_id = "dev5759", timeconstant=filter_tc, demod_freq=demod_freq), 1: MfliDriverChargeStability(dev_id = "dev5761", timeconstant=filter_tc, demod_freq=demod_freq), 2: MfliDriverChargeStability(dev_id = "dev6573", timeconstant=filter_tc, demod_freq=demod_freq)}
     mflis = {0: MfliDriverChargeStability(dev_id = "dev5759", timeconstant=filter_tc, demod_freq=demod_freq), 1: MfliDriverChargeStability(dev_id = "dev5761", timeconstant=filter_tc, demod_freq=demod_freq)}
-    print(mflis[0])
-    print(mflis[1].)
 
     gates = {"B1", "B2", "B3", "B4", "B5", "P1", "P2",  "P3", "P4", "L1", "L2",  "M1", "M2",  "R1", "R2",  "BS1", "BS2", "TS", "MS", "Source1", "Drain1", "Source2", "Drain2", "Source3", "Drain3"}
     lockin_configs = {1: {1,2,3}, 2: {1,2}, 3: {2,3}, 4: {1,3}, 5: {1}, 6: {2}, 7: {3}}
@@ -458,7 +456,7 @@ def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_
                              set_val(parameter2, V_y_f[j], channel_mapping, dac_server)
                              dac_server.close()
 
-                         else: 
+                         else:
                              dac_server = DacDriverSerialServer()
                              set_val(parameter2, V_y_f[j], channel_mapping, dac_server)
                              dac_server.close()
