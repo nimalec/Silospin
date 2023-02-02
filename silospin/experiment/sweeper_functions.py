d4a1c49cac0ed98c304a64946de94ec248190006
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-02 14:03:37 -0500

    dac

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -64,8 +64,8 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
                 set_val(parameter, v_in_array[j], dac_parameters, dac_server)
                 for idx in lockin_config:
                     V_out_lockins[idx].append(lockin_drivers[idx].get_sample_r())
-                dac_server.close_1()
-                dac_server.close_2()
+                # dac_server.close_1()
+                # dac_server.close_2()
                 if j%n_r == 0:
                     exec(plot_1)
                 else:
@@ -81,13 +81,13 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
             V_out_lockins = {}
             for idx in lockin_config:
                 V_out_lockins[idx] = []
-            for j in range(npoints): 
+            for j in range(npoints):
                 dac_server = DacDriverSerialServer()
                 set_val(parameter, v_in_array[j], dac_parameters, dac_server)
                 for idx in lockin_config:
                     V_out_lockins[idx].append(lockin_drivers[idx].get_sample_r())
-                dac_server.close_1()
-                dac_server.close_2()
+                # dac_server.close_1()
+                # dac_server.close_2()
 
             for idx in lockin_config:
                 V_out_average[idx].append(V_out_lockins[idx])
