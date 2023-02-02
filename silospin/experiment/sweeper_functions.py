2d012e4924cb1e425cda44c02ed8295fecb3b154
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-01 21:46:10 -0500

    dac

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -61,19 +61,21 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
             V_out_lockins = {}
             for idx in lockin_config:
                 V_out_lockins[idx] = []
-                for j in range(npoints):
-                    if parameter == "channel_voltage_set" or parameter == "gates_voltages_set":
-                        pass
-                    else:
-                        #dac_server = DacDriverSerialServer()
-                        #set_val(parameter, v_in_array[j], channel_mapping, dac_server)
-                        for idx in lockin_config:
-                            print(V_out_lockins[idx])
-                            V_out_lockins[idx].append(lockin_drivers[idx].get_sample_r())
-                        #dac_server.close()
-                        if j%n_r == 0:
-                            print(plot_1)
-                            exec(plot_1)
+            for j in range(npoints):
+                # if parameter == "channel_voltage_set" or parameter == "gates_voltages_set":
+                #     pass
+                # else:
+                #dac_server = DacDriverSerialServer()
+                #set_val(parameter, v_in_array[j], channel_mapping, dac_server)
+                for idx in lockin_config:
+                    print(V_out_lockins[idx])
+                    V_out_lockins[idx].append(lockin_drivers[idx].get_sample_r())
+                #dac_server.close()
+                if j%n_r == 0:
+                    print(plot_1)
+                    exec(plot_1)
+                else:
+                    pass
 
             for idx in lockin_config:
                 V_out_average[idx].append(V_out_lockins[idx])
