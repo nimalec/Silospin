76b6c43173209d6d2dfaab75139ee16654c38d03
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-02 17:42:46 -0500

    dac

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -97,38 +97,117 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
 
     return return_value
 #
-# def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_value2, end_value2, npoints2, n_r = 10, n_fr = 1, plot = True, lockins = {1,2,3}, filter_tc=10e-3, demod_freq = 0, dac_mapping_file_path = 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac.pickle', save_path=None):
-#     '''
-#     Outputs the mapping between AWG cores/channels and gate labels. \n
-#     Outer keys of dictionary correspond to core number running from 1-4 (e.g. chanel_mapping = {1 : {}, ... , 4: {}). These keys have values in the form of dictonaries with the following keys and values. \n
-#     - "ch", dictionary of the core's output channels, output gate labels, and gate indices in the GST convention (dict) \n
-#     - "rf", 1 if core is for RF pulses and 0 if for DC pulses (int) \n
-#     The dictionaries corresponding to the key "ch" have the following keys and values,
-#     - "index", list of 2 channels corresponding the specified core (grouping given by- 1: [1,2], 2: [3,4], 3: [5,6], 4: [7,8]). \n
-#     - "label", list of 2 labels corresponding to each channel (e.g. ["i1", "q1"] as IQ pair for RF or  ["p12", "p21"] as 2 plunger channels for DC). \n
-#     - "gateindex", list of 2 gate indices corresponding to GST indices. (e.g. gate (1)x(1) maps to gateindex [1,1] for core 1 or (7)p(7)(8)p(8) maps to indices [7,8] of core 4.)\n
-#     Note: currently configured for 1 HDAWG unit with 4 AWG cores.   \n
-#     Parameters:
-#                     rf_cores (list): list of core indices dedicated to RF control (default set to [1,2,3]).
-#                     plunger_channels (dict): dictionary of RF core (default set to  {"p12": 7, "p21": 8})).
-#     Returns:
-#        channel_mapper (dict): Dictionary representing channel mapping.
-#     '''
-#     dac_server = DacDriverSerialServer()
-#     #mflis = {0: MfliDriverChargeStability(dev_id = "dev5759", timeconstant=filter_tc, demod_freq=demod_freq), 1: MfliDriverChargeStability(dev_id = "dev5761", timeconstant=filter_tc, demod_freq=demod_freq), 2: MfliDriverChargeStability(dev_id = "dev6573", timeconstant=filter_tc, demod_freq=demod_freq)}
-#     mflis = {0: MfliDriverChargeStability(dev_id = "dev5759", timeconstant=filter_tc, demod_freq=demod_freq, sig_path="/dev5759/demods/0/sample"), 1: MfliDriverChargeStability(dev_id = "dev5761", timeconstant=filter_tc, demod_freq=demod_freq, sig_path="/dev5761/demods/0/sample")}
-#     gates = {"B1", "B2", "B3", "B4", "B5", "P1", "P2",  "P3", "P4", "L1", "L2",  "M1", "M2",  "R1", "R2",  "BS1", "BS2", "TS", "MS", "Source1", "Drain1", "Source2", "Drain2", "Source3", "Drain3"}
-#     lockin_configs = {1: {1,2,3}, 2: {1,2}, 3: {2,3}, 4: {1,3}, 5: {1}, 6: {2}, 7: {3}}
-#     dac_dict = unpickle_qubit_parameters(dac_mapping_file_path)
-#     channel_mapping = dac_dict["channel_mapping"]
-#
-#     v_x = np.linspace(start_value1, end_value1, npoints1)
-#     v_y = np.linspace(start_value2, end_value2, npoints2)
-#     V_x, V_y = np.meshgrid(v_x, v_y)
-#     V_x_f = V_x.flatten()
-#     V_y_f = V_y.flatten()
-#
-#
+def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_value2, end_value2, npoints2, n_r = 10, n_fr = 1, plot = True, lockins = {1,2,3}, filter_tc=10e-3, demod_freq = 0, dac_mapping_file_path = 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac.pickle', save_path=None):
+    '''
+    Outputs the mapping between AWG cores/channels and gate labels. \n
+    Outer keys of dictionary correspond to core number running from 1-4 (e.g. chanel_mapping = {1 : {}, ... , 4: {}). These keys have values in the form of dictonaries with the following keys and values. \n
+    - "ch", dictionary of the core's output channels, output gate labels, and gate indices in the GST convention (dict) \n
+    - "rf", 1 if core is for RF pulses and 0 if for DC pulses (int) \n
+    The dictionaries corresponding to the key "ch" have the following keys and values,
+    - "index", list of 2 channels corresponding the specified core (grouping given by- 1: [1,2], 2: [3,4], 3: [5,6], 4: [7,8]). \n
+    - "label", list of 2 labels corresponding to each channel (e.g. ["i1", "q1"] as IQ pair for RF or  ["p12", "p21"] as 2 plunger channels for DC). \n
+    - "gateindex", list of 2 gate indices corresponding to GST indices. (e.g. gate (1)x(1) maps to gateindex [1,1] for core 1 or (7)p(7)(8)p(8) maps to indices [7,8] of core 4.)\n
+    Note: currently configured for 1 HDAWG unit with 4 AWG cores.   \n
+    Parameters:
+                    rf_cores (list): list of core indices dedicated to RF control (default set to [1,2,3]).
+                    plunger_channels (dict): dictionary of RF core (default set to  {"p12": 7, "p21": 8})).
+    Returns:
+       channel_mapper (dict): Dictionary representing channel mapping.
+    '''
+    itr = 1
+    lockin_drivers = {}
+    for idx in range(len(lockins)):
+        lockin_drivers[itr] = MfliDriverChargeStability(dev_id = lockins[itr], timeconstant=filter_tc, demod_freq=demod_freq, sig_path= f"/{lockins[itr]}/demods/0/sample")
+        itr += 1
+
+    ##Make connection to DAC server
+    dac_server = DacDriverSerialServer()
+    dac_parameters = {}
+    for idx in dac_settings:
+        dac_dict = unpickle_qubit_parameters(dac_settings[idx])
+        dac_parameters[idx] = dac_dict
+
+    V_out_average = {}
+    for idx in lockin_config:
+        V_out_average[idx] = []
+
+    V_out_lockins = {}
+    for idx in lockin_config:
+        V_out_lockins[idx] = np.ones((npoints1,npoints2)).flatten()
+
+    v_x = np.linspace(start_value1, end_value1, npoints1)
+    v_y = np.linspace(start_value2, end_value2, npoints2)
+    V_x, V_y = np.meshgrid(v_x, v_y)
+    V_x_f = V_x.flatten()
+    V_y_f = V_y.flatten()
+
+    #                          img1 = ax1.imshow(V_out1, extent=[start_value1,end_value1,start_value2,end_value2])
+    #                          ax1.set_xlabel(parameter1+" gate voltage [V]")
+    #                          ax1.set_ylabel(parameter2+" gate voltage [V]")
+    #                          fig1.canvas.draw()
+    #                          cbar1 = fig1.colorbar(img1, ax=ax1, extend='both')
+    #                          cbar1.set_label('Demodulated voltage from lock-in 1 [V]', rotation=270, labelpad=30)
+    #                          plt.show(block=False)
+
+    if plot == True:
+        fig_str = ''
+        plot_0 = ''
+        plot_1 = ''
+        plot_2 = ''
+        plot_3 = ''
+        for idx in lockin_config:
+            fig_str += f'fig{idx} = plt.figure()\nax{idx}=fig{idx}.add_subplot(111)\n'
+            plot_0 += f'img{idx} = ax{idx}.imshow(V_out_lockins[{idx}].reshape([npoints1, npoints2]).T, extent=[start_value1,end_value1,end_value2,start_value2])\nax{idx}.set_xlabel(parameter1+" gate voltage [V]")\nax{idx}.set_ylabel(parameter2+" gate voltage [V]")\nfig{idx}.canvas.draw()\ncbar{idx} = fig{idx}.colorbar(img{idx}, ax=ax{idx}, extend="both")\ncbar{idx}.set_label("Demodulated voltage from lock-in {idx} [V]", rotation=270, labelpad=30)'
+            plot_1 = f'cbar{idx}.remove()\ncbar{idx} = fig{idx}.colorbar(img{idx}, ax=ax{idx}, extend="both")\n'
+            plot_2 = f'img{idx}.set_data(V_out_lockins[{idx}].reshape([npoints1, npoints2]).T\nimg{idx}.set_clim(np.amin(V_out_lockins[{idx}]), np.amax(V_out_lockins[{idx}]))\n'
+            plot_3 = f'fig{idx}.canvas.draw()\nplt.show(block=False){idx}\nfig{idx}.canvas.flush_events()\n'
+        exec(fig_str)
+
+        for i in range(n_fr):
+            V_out_lockins = {}
+            for idx in lockin_config:
+                V_out_lockins[idx] = np.ones((npoints1,npoints2)).flatten()
+
+                for j in range(len(V_x_f)):
+                    if j == 0:
+                        set_val(parameter1, V_x_f[j], channel_mapping, dac_server)
+                        set_val(parameter2, V_y_f[j], channel_mapping, dac_server)
+
+                        for idx in lockin_config:
+                            v_meas = lockin_drivers[idx].get_sample_r()
+                            V_out_lockins[idx] = v_meas*V_out_lockins[idx]
+
+                        exec(plot_0)
+                        if i > 0:
+                            exec(plot_1)
+                        else:
+                            pass
+                    else:
+                        set_val(parameter1, V_x_f[j], channel_mapping, dac_server)
+                        set_val(parameter2, V_y_f[j], channel_mapping, dac_server)
+                        for idx in lockin_config:
+                            V_out_lockins[idx][j] = lockin_drivers[idx].get_sample_r()
+
+                        if j%n_r == 0:
+                            exec(plot_1)
+                            exec(plot_2)
+                        else:
+                            pass
+                    for idx in lockin_config:
+                        V_out_average[idx].append(V_out_lockins[idx])
+
+        return_value = {}
+        return_value["v_applied"] = v_in_array.tolist()
+        for idx in lockin_config:
+            return_value[f'v_out{idx}'] =  np.mean(np.array(V_out_average[idx]),axis=0)
+
+#         if plot == True:
+#             fig1 = plt.figure()
+#             ax1 = fig1.add_subplot(111)
+#             fig2 = plt.figure()
+#             ax2 = fig2.add_subplot(111)
+#             fig3 = plt.figure()
+#             ax3 = fig3.add_subplot(111)
 #     ## All lockins simultaneous: 1,2,3.
 #     if lockins == lockin_configs[1]:
 #         V_out_all_1 = []
@@ -412,4 +491,4 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
 #
 #     else:
 #         pass
-#     return return_value
+    return return_value
