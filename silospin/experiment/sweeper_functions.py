4435f8c94c64cbffc5efa651b83c7b29ba5d22f0
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-02 22:09:29 -0500

    plot

--- silospin/experiment/sweeper_functions.py
+++ silospin/experiment/sweeper_functions.py
@@ -8,7 +8,7 @@ from itertools import permutations
 import numpy as np
 import matplotlib.pyplot as plt
 
-def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, plot = True, lockin_config = (1,2), lockins = {1: 'dev5759', 2: 'dev5761'}, dac_settings = {1: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac1.pickle', 2: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac2.pickle'}, filter_tc=10e-3, demod_freq =0, save_path=None):
+def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, plot = True, lockin_config = [1,2], lockins = {1: 'dev5759', 2: 'dev5761'}, dac_settings = {1: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac1.pickle', 2: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac2.pickle'}, filter_tc=10e-3, demod_freq =0, save_path=None):
     '''
     Outputs the mapping between AWG cores/channels and gate labels. \n
     Outer keys of dictionary correspond to core number running from 1-4 (e.g. chanel_mapping = {1 : {}, ... , 4: {}). These keys have values in the form of dictonaries with the following keys and values. \n
@@ -98,7 +98,7 @@ def do1DSweep(parameter, start_value, end_value, npoints, n_r = 10, n_fr = 1, pl
 
     return return_value
 
-def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_value2, end_value2, npoints2, n_r = 10, n_fr = 1, plot = True, lockin_config = (1,2), lockins = {1: 'dev5759', 2: 'dev5761'},  dac_settings = {1: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac1.pickle', 2: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac2.pickle'}, filter_tc=10e-3, demod_freq = 0, save_path=None):
+def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_value2, end_value2, npoints2, n_r = 10, n_fr = 1, plot = True, lockin_config = [1,2], lockins = {1: 'dev5759', 2: 'dev5761'},  dac_settings = {1: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac1.pickle', 2: 'C:\\Users\\Sigillito Lab\\Desktop\\experimental_workspaces\\quantum_dot_workspace_bluefors1\\experiment_parameters\\bluefors1_dac2.pickle'}, filter_tc=10e-3, demod_freq = 0, save_path=None):
     '''
     Outputs the mapping between AWG cores/channels and gate labels. \n
     Outer keys of dictionary correspond to core number running from 1-4 (e.g. chanel_mapping = {1 : {}, ... , 4: {}). These keys have values in the form of dictonaries with the following keys and values. \n
@@ -188,13 +188,49 @@ def do2DSweep(parameter1, start_value1, end_value1, npoints1, parameter2, start_
                         exec(plot_3)
                     else:
                         pass
-        #             for idx in lockin_config:
-        #                 V_out_average[idx].append(V_out_lockins[idx])
-        #
-        # return_value = {}
-        # return_value["v_applied"] = v_in_array.tolist()
-        # for idx in lockin_config:
-        #     return_value[f'v_out{idx}'] =  np.mean(np.array(V_out_average[idx]),axis=0)
+                    for idx in lockin_config:
+                        V_out_average[idx].append(V_out_lockins[idx])
+
+        return_value = {}
+        return_value["v_applied"] = v_in_array.tolist()
+        for idx in lockin_config:
+            return_value[f'v_out{idx}'] =  np.mean(np.array(V_out_average[idx]),axis=0)
+    else:
+        for i in range(n_fr):
+            set_val(parameter1, V_x_f[0], dac_parameters, dac_server)
+            set_val(parameter2, V_y_f[0], dac_parameters, dac_server)
+            V_out_lockins = {}
+            for idx in lockin_config:
+                V_out_lockins[idx] = np.ones((npoints1,npoints2)).flatten()
+
+            for j in range(len(V_x_f)):
+                if j == 0:
+                    set_val(parameter1, V_x_f[j], dac_parameters, dac_server)
+                    set_val(parameter2, V_y_f[j], dac_parameters, dac_server)
+
+                    for idx in lockin_config:
+                        v_meas = lockin_drivers[idx].get_sample_r()
+                        V_out_lockins[idx] = v_meas*V_out_lockins[idx]
+                else:
+                    set_val(parameter1, V_x_f[j], dac_parameters, dac_server)
+                    set_val(parameter2, V_y_f[j], dac_parameters, dac_server)
+                    for idx in lockin_config:
+                        V_out_lockins[idx][j] = lockin_drivers[idx].get_sample_r()
+
+                    for idx in lockin_config:
+                        V_out_average[idx].append(V_out_lockins[idx])
+
+        return_value = {}
+        return_value["v_applied"] = v_in_array.tolist()
+        for idx in lockin_config:
+            return_value[f'v_out{idx}'] =  np.mean(np.array(V_out_average[idx]),axis=0)
+    return return_value
+
+
+
+
+
+
 
 #         if plot == True:
 #             fig1 = plt.figure()
