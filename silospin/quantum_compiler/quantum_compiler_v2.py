dcc71172b55f0e0762a04ca0eefd73694f6b3434
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-07 22:02:35 -0500

    awg

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -429,9 +429,12 @@ class GateSetTomographyQuantumCompiler:
                 "messages"
                 ], f"There was an error during compilation: {compiler_info['messages']}"
 
+                daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/elf/data", elf)
+                assert(daq.getDouble(f"/{device_id}/awgs/0/elf/progress") == 100.0), ""
+
                 for wave_idx in waveforms_to_awg[awg_idx][core_idx]:
                     daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
-
+                #
                 # daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/elf/data", elf)
                 # assert(daq.getDouble(f"/{device_id}/awgs/0/elf/progress") == 100.0), ""
 
