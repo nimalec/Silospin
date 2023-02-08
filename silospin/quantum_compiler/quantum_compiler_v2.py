1d9981d7ff20381666c001fe17e95e4d49640ef2
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-07 22:49:10 -0500

    awg

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -440,10 +440,10 @@ class GateSetTomographyQuantumCompiler:
 
                 elf, info = self._awgs[awg_idx]._hdawg.awgs[core_idx-1].compile_sequencer_program(sequence_program.code)
 
-                for wave_idx in waveforms_to_awg[awg_idx][core_idx]:
-                    print(core_idx, wave_idx)
-                    daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
-
+                # for wave_idx in waveforms_to_awg[awg_idx][core_idx]:
+                #     print(core_idx, wave_idx)
+                #     daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
+                #
 
                 # daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/elf/data", elf)
                 # assert(daq.getDouble(f"/{device_id}/awgs/0/elf/progress") == 100.0), ""
