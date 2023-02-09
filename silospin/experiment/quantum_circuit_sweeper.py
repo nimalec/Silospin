04e082c33e0c4caed2dd1ecfee32ceb9a8e8c2c5
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-09 12:48:22 -0500

    rabi

--- silospin/experiment/quantum_circuit_sweeper.py
+++ silospin/experiment/quantum_circuit_sweeper.py
@@ -9,9 +9,9 @@ def make_rabisweep_file(qubits, start_tau, end_tau, npoints, amplitude, filepath
         gate_str = f"{amp_str}*X[{tau_str}&0&0]"
         temp_line = ""
         for q in qubits:
-            gate_line = f"({str(q)}){gate_str}({str(q)});"
+            gate_line = f"({str(q)}){gate_str}({str(q)})"
             temp_line += gate_line
-        temp_line = temp_line+'\n'
+        temp_line = temp_line+';\n'
         file_str += temp_line
 
     with open(filepath, 'w') as f:
