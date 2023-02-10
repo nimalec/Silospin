2720fafa40cfc75740bab59eb86ebefac6bbc3f6
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-10 14:51:33 -0500

    qc

--- silospin/quantum_compiler/quantum_compiler_v2.py
+++ silospin/quantum_compiler/quantum_compiler_v2.py
@@ -413,9 +413,9 @@ class GateSetTomographyQuantumCompiler:
                 elf, info = self._awgs[awg_idx]._hdawg.awgs[core_idx-1].compile_sequencer_program(sequence_program.code)
                 self._awgs[awg_idx]._hdawg.awgs[core_idx-1].elf.data(elf)
 
-                for wave_idx in waveforms_to_awg[awg_idx][core_idx]:
-                    daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
-                self._awgs[awg_idx]._hdawg.awgs[core_idx-1].commandtable.upload_to_device(self._command_tables[awg_idx][core_idx])
+                # for wave_idx in waveforms_to_awg[awg_idx][core_idx]:
+                #     daq.setVector(f"/{device_id}/awgs/"+str(core_idx-1)+"/waveform/waves/"+str(wave_idx), waveforms_to_awg[awg_idx][core_idx][wave_idx])
+                # self._awgs[awg_idx]._hdawg.awgs[core_idx-1].commandtable.upload_to_device(self._command_tables[awg_idx][core_idx])
 
     def compile_program(self):
         """
