0a01c9ebc4cf3b0053db1110ae85cfbfe4c1fb8c
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-21 22:17:11 -0500

    qc

--- silospin/experiment/setup_quantumalgo_instruments.py
+++ silospin/experiment/setup_quantumalgo_instruments.py
@@ -17,7 +17,7 @@ def initialize_drivers(awgs, lockins, rf_dc_core_grouping, trig_channels):
 
     itr = 0
     for awg in rf_dc_core_grouping:
-        drivers_str += f'awg_driver_{itr+1}=HdawgDriver(awgs[{itr}], {awg} , channel_mapping, awg_mapping)\n'
+        drivers_str += f'awg_driver_{itr+1}=HdawgDriver(awgs[{itr}], {quote}{awg}{quote} , channel_mapping, awg_mapping)\n'
         itr += 1
 
     for mfli in lockins:
