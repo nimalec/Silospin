c6fbe0d191476f097ef9a9c346281983dd871309
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-23 16:05:57 -0500

    awg

--- silospin/drivers/mfli_triggered.py
+++ silospin/drivers/mfli_triggered.py
@@ -444,6 +444,7 @@ class MfliDaqModule:
         columns = np.ceil(duration*sample_rate)
         self._daq_module.set("grid/cols", columns)
         self._daq_module.set('grid/rows', rows)
+        self._daq_module.set("count", 1) 
 
         # sig_source = {'Demod_R': f'/{self._dev_id}/demods/0/sample.R' , 'Aux_in_1': f'/{self._dev_id}/demods/0/sample.AuxIn0'}
         # self._daq_module.subscribe(sig_source[sig_port])
