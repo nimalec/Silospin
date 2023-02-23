d873317e81776e00ec35bbbf6ffd430bd9c8e4db
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-23 17:07:21 -0500

    awg

--- silospin/experiment/quantumalgo_experiment.py
+++ silospin/experiment/quantumalgo_experiment.py
@@ -82,6 +82,7 @@ class QuantumAlgoExperiment:
 
         for i in range(self._n_trigger):
             #t_0 = time.time()
+            print(i)
             for daq in self._daq_modules:
                 self._daq_modules[daq].enable_triggered_data_acquisition_time_domain(self._measurement_settings['acquisition_time'], self._measurement_settings['sample_rate'], rows = 1 ,sig_port  = sig_port)
                 self._daq_modules[daq]._daq_module.execute()
