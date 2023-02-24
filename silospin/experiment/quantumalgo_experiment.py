c2d9287a2a6e7898771ca817a55c5b915157e058
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-24 15:27:58 -0500

    dac

--- silospin/experiment/quantumalgo_experiment.py
+++ silospin/experiment/quantumalgo_experiment.py
@@ -11,7 +11,7 @@ from silospin.drivers.mfli_triggered import MfliDaqModule
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from multiprocessing import Process   
+from multiprocessing import Process
 
 class QuantumAlgoExperiment:
     ##Only utilizes Trigbox, MFLIs. Sweeper modules will utilize this...
@@ -80,7 +80,7 @@ class QuantumAlgoExperiment:
         processes = []
         for daq in self._daq_modules:
             daq_mod = self._daq_modules[daq]
-            process = multiprocessing.Process(target=mflitrig_daq_helper, args=(daq_mod, self._n_trigger, self._measurement_settings['sample_rate'], self._sig_port))
+            process = multiprocessing.Process(target=mflitrig_daq_helper, args=(daq_mod, self._n_trigger, self._measurement_settings['acquisition_time'], self._measurement_settings['sample_rate'], self._sig_port))
             process.start()
             processes.append(process)
 
