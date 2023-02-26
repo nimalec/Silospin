3cfbea183a5815b05ae545e01c70a510fa38b140
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-26 14:01:05 -0500

    trig box

--- silospin/experiment/quantumalgo_experiment.py
+++ silospin/experiment/quantumalgo_experiment.py
@@ -137,11 +137,13 @@ class QuantumAlgoExperiment:
 
         while not self._daq_modules[0]._daq_module.finished():
 #        for i in range(self._n_trigger):
+            print('tic')
             self._trig_box.send_trigger()
             t_0 = time.time()
             #data_read = self._daq_modules[daq]._daq_module.read(True)
             data_read = self._daq_modules[0]._daq_module.read(True)
-            #ime.sleep(.1)
+            time.sleep(1)
+            print('toc')
             if self._sig_source[0][sig_port].lower() in data_read.keys():
                 #if self._sig_source[daq][sig_port].lower() in data_read.keys():
                 # min_val = np.amin(data_read[self._sig_source[daq][sig_port].lower()][0]['value'][0]) - abs(np.amin(data_read[self._sig_source[daq][sig_port].lower()][0]['value'][0]))/5
