b05ecf5eeee6d375812b3839ccd6fda137770c87
nimlaec <47722154+nimalec@users.noreply.github.com>
2023-02-25 22:14:52 -0500

    trig box

--- silospin/experiment/quantumalgo_experiment.py
+++ silospin/experiment/quantumalgo_experiment.py
@@ -152,7 +152,7 @@ class QuantumAlgoExperiment:
                     #self._sample_data[daq].append(sig)
                     self._sample_data[0].append(sig)
         data_read = self._daq_modules[0]._daq_module.read(True)
-        if sig_source[sig_port].lower() in data_read.keys():
+        if self._sig_source[0][sig_port].lower() in data_read.keys():
             for each in data_read[sig_source[sig_port].lower()]:
                 self._sample_data.append(each)
 
